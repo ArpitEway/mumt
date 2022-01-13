6bb099a95f7b84265a533040a6103d7310a93295
Shubha1232 <95339673+Shubha1232@users.noreply.github.com>
2022-01-13 17:59:54 +0530

    add loader

--- application/views/admin/enrollment/consolidate_report.php
+++ application/views/admin/enrollment/consolidate_report.php
@@ -137,13 +137,10 @@ List
 
 </div>
 
-<div align="center" id="myLoader" class="loader_div" style="display: none;" >
-  <svg>
-    <circle cx="50" cy="50" r="40" stroke="red" stroke-dasharray="78.5 235.5" stroke-width="3" fill="none" />
-    <circle cx="50" cy="50" r="30" stroke="blue" stroke-dasharray="62.8 188.8" stroke-width="3" fill="none" />
-    <circle cx="50" cy="50" r="20" stroke="green" stroke-dasharray="47.1 141.3" stroke-width="3" fill="none" />
-  </svg>
+
 </div>
+<div id="loader">
+	</div>
 <div class="form-group text-center">
 	<button class="btn btn-md btn-primary mt-4" type="button" id="submit_btn">Submit</button>
 </div>
@@ -157,8 +154,8 @@ List
 <script>
 
 $(document).on("click","#submit_btn",function(){
-	alert('fd');
 	
+	$('#loader').addClass('loading');
 	var csrfName = $('.csrfname').attr('name');
 		var csrfHash = $('.csrfname').val(); 
 	var data = {
@@ -181,19 +178,14 @@ $(document).on("click","#submit_btn",function(){
                 type:'post',
                 dataType : 'JSON',
                 data:data,
-                 beforeSend: function()
-              {
-                $("#myLoader").show();
-               },
+               
                 success:function(status)
                 {
+					$('#loader').removeClass('loading');
 					$('#dt').html(status.data);
 	               KTDatatablesBasicBasic.init();
                 },
-                   complete: function()
-              {
-                $('#myLoader').hide();
-              },
+                  
             })
 	
 });
