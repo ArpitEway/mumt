694f8a7bfdc80538d0a8f7d1bfd9b4e431204b87
akshay nagar <akshay.eway@gmail.com>
2022-02-04 13:16:17 +0530

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,19 +49,54 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Dashboard';
 
+
+if($_SERVER['HOSTNAME']=='admin.mmyvvonline.com'){
 
 /* Admin Routes   */
-$route['admin'] = 'admin/Admins/login';
-$route['admin/dashboard'] = 'admin/Admins/dashboard';
-$route['admin/login'] = 'admin/Admins/login';
-$route['admin/loginSub'] = 'admin/Admins/loginSub';
-$route['admin/Admin'] = 'admin/Admins';
-$route['admin/logout'] = 'admin/Admins/logout';
-$route['admin/enrollment/show_form/(:num)'] = 'admin/enrollment/show_form/$1';
-$route['admin/updateStudentData/(:num)'] = 'admin/updateStudentData/index/$1';
-$route['admin/remote_login/(:any)'] = 'admin/Admins/remote_login/$1';
+$route[''] = 'admin/Admins/login';
+$route['dashboard'] = 'admin/Admins/dashboard';
+$route['login'] = 'admin/Admins/login';
+$route['loginSub'] = 'admin/Admins/loginSub';
+$route['Admin'] = 'admin/Admins';
+$route['logout'] = 'admin/Admins/logout';
+$route['enrollment/show_form/(:num)'] = 'admin/enrollment/show_form/$1';
+$route['updateStudentData/(:num)'] = 'admin/updateStudentData/index/$1';
+$route['remote_login/(:any)'] = 'admin/Admins/remote_login/$1';
+
+}elseif($_SERVER['HOSTNAME']=='center.mmyvvonline.com'){
+
+
+$route[''] = 'center/center';
+$route['profile'] = 'center/center/profile';
+$route['change_password'] = 'center/center/change_password';
+$route['login'] = 'center/center/login';
+$route['logout'] = 'center/center/logout';
+$route['dashboard'] = 'center/center/dashboard';
+$route['instruction'] = 'center/center/instruction';
+$route['admission_form'] = 'center/center/admission_form';
+$route['all_student'] = 'center/center/all_student';
+$route['show_form/(:any)'] = 'center/center/show_form/$1';
+$route['show_fees/(:any)'] = 'center/center/show_fees/$1';
+$route['student_list/(:any)'] = 'center/center/student_list/$1';
+$route['admission_instruction'] = 'center/center/admission_instruction';
+$route['loginAs/(:any)'] = 'center/center/loginAs/$1';
+$route['not_approve_student_list'] = 'center/center/not_approve_student_list';
+$route['remaining_documents']  = 'center/center/remaining_documents';
+$route['Document/remainingDocument'] = 'center/Document/remainingDocument';
+$route['form_edit_request']  = 'center/center/form_edit_request';
+$route['get_request_detail'] = 'center/center/get_request_detail';
+$route['payment_complaint'] = 'center/center/payment_complaint';
+
+}
+
+
+
+
+
+
+
+$route['default_controller'] = 'Dashboard';
 
 $route['student'] = 'student/Student';
 $route['student/login'] = 'student/Student/login';
@@ -74,29 +109,6 @@ $route['student/form'] = 'student/Student/form';
 $route['student/admission/(:num)'] = 'student/Student/admission/$1';
 $route['student/enquiry'] = 'student/Student/enquiry';
 $route['student/new_admission'] = 'student/Student/new_admission';
-
-
-$route['center'] = 'center/center';
-$route['center/profile'] = 'center/center/profile';
-$route['center/change_password'] = 'center/center/change_password';
-$route['center/login'] = 'center/center/login';
-$route['center/logout'] = 'center/center/logout';
-$route['center/dashboard'] = 'center/center/dashboard';
-$route['center/instruction'] = 'center/center/instruction';
-$route['center/admission_form'] = 'center/center/admission_form';
-$route['center/all_student'] = 'center/center/all_student';
-$route['center/show_form/(:any)'] = 'center/center/show_form/$1';
-$route['center/show_fees/(:any)'] = 'center/center/show_fees/$1';
-$route['center/student_list/(:any)'] = 'center/center/student_list/$1';
-$route['center/admission_instruction'] = 'center/center/admission_instruction';
-$route['center/loginAs/(:any)'] = 'center/center/loginAs/$1';
-$route['center/not_approve_student_list'] = 'center/center/not_approve_student_list';
-$route['center/center/remaining_documents']  = 'center/center/remaining_documents';
-$route['center/Document/remainingDocument'] = 'center/Document/remainingDocument';
-$route['center/form_edit_request']  = 'center/center/form_edit_request';
-$route['center/get_request_detail'] = 'center/center/get_request_detail';
-$route['center/payment_complaint'] = 'center/center/payment_complaint';
-
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
