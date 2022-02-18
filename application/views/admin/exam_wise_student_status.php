85d9337719d0e3197642ddd9272d3f4a6949dab4
akshay nagar <akshay.eway@gmail.com>
2022-02-18 16:36:26 +0530

    update changes

--- application/views/admin/exam_wise_student_status.php
+++ application/views/admin/exam_wise_student_status.php
@@ -1,29 +1,28 @@
 <table id="" class="table table-striped dt-responsive" width="100%" >
-<thead>
+	<thead>
 		<tr>
-					<th>Sno.</th>
-					<th>Total Exam Form</th>
-					<th>Total Exam Form Fill</th>
-					<th>Total Exam Form Skip</th>
-					<th>Total Exam Form Remaining </th>
-		
+			<th>Sno.</th>
+			<th>Total Exam Form</th>
+			<th>Fill Exam Form</th>
+			<th>Skip Exam Form</th>
+			<th>Remaining Exam Form</th>
 		</tr>
-</thead>
-<tbody>      
- <?php
-                 $i=1;
-                 {?>
-		
-				<tr>
+	</thead>
+	<tbody>      
+		<?php
+		$i=1;
+		{
+			?>
+			<tr>
 				<td><?php echo $i; ?></td>
 				<td><?php echo $permitted_student;?></td>
 				<td><?php echo $filled_student; ?></td>
 				<td><?php echo $skipped_student; ?></td>
-				<td> <a target="_blank" href="<?= base_url('admin/admins/center_wise_remains_count/');?>" ><?php echo $not_filled_student; ?></a>
-			</td>
+				<td> <a target="_blank" href="<?= base_url('center_wise_remains_count');?>" ><?php echo $not_filled_student; ?></a>
+				</td>
 			</tr>
 			<?php 
-		}?>
-		
-</tbody>
+			}
+		?>
+	</tbody>
 </table>
