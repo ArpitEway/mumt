7b19c45bf515a167dc45066cd17489d6b8e8593c
akshay nagar <akshay.eway@gmail.com>
2022-02-19 14:58:03 +0530

    Update Center.php

--- application/controllers/center/Center.php
+++ application/controllers/center/Center.php
@@ -112,13 +112,13 @@ class Center extends CI_Controller {
 
 	public function logout()
 	{
-		$center_id = array( 10,11,12,13 );
+		$center_ids = array( 10,11,12,13 );
 		if(in_array($this->session->center_id, $center_ids)){
-		$this->session->sess_destroy();
-		redirect('http://162.144.38.91/~mmyvvdde/main/center/index.php');
-		}else{
-		$this->session->sess_destroy();
+			$this->session->sess_destroy();
 			redirect(base_url());
+		}else{
+			$this->session->sess_destroy();
+			redirect('http://162.144.38.91/~mmyvvdde/main/center/index.php');
 		}
 	}
 
