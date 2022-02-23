df59795aae5d076f21cfb4844369c180757ac710
akshay nagar <akshay.eway@gmail.com>
2022-02-23 15:41:28 +0530

    Update answersheet_uplaod_status.php

--- application/views/admin/answersheet_uplaod_status.php
+++ application/views/admin/answersheet_uplaod_status.php
@@ -1,18 +1,20 @@
-<table class="table text-uppercase">
-	<thead>
-		<tr>
-			<th>Total</th>
-			<th>Uploaded</th>
-			<th>Rremaining</th>
-			<th>Checked</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td><?php echo  $total_paper_count ; ?></td>
-			<td><?php echo $uploaded ; ?></td>
-			<td><?php  echo $total_paper_count-$uploaded ; ?></td>
-			<td><?php echo $checked ; ?></td>
-		</tr>
-	</tbody>
-</table>
+<div class="container">
+	<table class="table text-uppercase">
+		<thead>
+			<tr>
+				<th>Total</th>
+				<th>Uploaded</th>
+				<th>Rremaining</th>
+				<th>Checked</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><?php echo  $total_paper_count ; ?></td>
+				<td><?php echo $uploaded ; ?></td>
+				<td><?php  echo $total_paper_count-$uploaded ; ?></td>
+				<td><?php echo $checked ; ?></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
