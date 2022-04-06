6e3a8aedbbd1f767614fe9877cc78ea6b556be41
nitinsharmaeway@gmail.com <95339673+Shubha1232@users.noreply.github.com>
2022-04-06 11:55:27 +0530

    Update activity_img_popup.php

--- application/views/Centers/activity/activity_img_popup.php
+++ application/views/Centers/activity/activity_img_popup.php
@@ -3,42 +3,29 @@
         //    echo "<pre>";
         //    print_r($activity_file);
  ?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
- <style>
-     .gallary {
-         margin : 10px 50px ;
-     }
-     .gallary img {
-       transition : 1s ;
-       padding : 15px ;
-       width : 200px ;
-     }
-     .gallary img:hover {
-         filter : grayscale(100%);
-         transform : scale(1.1);
-     }
-     /* .lb-data .lb-close {
-    position: relative;
-    top: 38px;
-    left: 38px;
-    z-index: 999; */
-}
- </style>
-<div class="gallary">
-    <?php  
-            foreach($activity_file as $image){
+ <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<div class="">
+  <div class=" w3-third" style="display:flex">
+  <?php  
+  foreach($activity_file as $image){
     ?>
-     <a   href="<?=base_url('assets/activity/'.$image->activity_file)?>"  data-lightbox="photos" ><img   src="<?=base_url('assets/activity/'.$image->activity_file)?>" alt="" style="padding: 15px 10px;  width:  100px; object-fit: cover; height: 100px;"></a>
+   <img  onclick="onClick(this)"  src="<?=base_url('assets/activity/'.$image->activity_file)?>" alt=""  style=" cursor:pointer ;padding: 15px 10px;height: 100px;">
     <?php
-            }
+    }
     ?>
+  </div>
+</div>
+
+<div id="modal01" class="w3-modal" onclick="this.style.display='none'">
+  <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+  <div style="width:50%;height:50%" class="w3-modal-content  w3-animate-zoom">
+    <img id="img01" style="width:100%">
+  </div>
 </div>
 <script>
-    // lightbox.option({
-    //   'resizeDuration': 200,
-    //   'wrapAround': true
-    // })
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
 </script>
+            
