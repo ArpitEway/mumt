39a0c3eab43d7dfd7e22a591510fee9d2824b0c5
akshay nagar <akshay.eway@gmail.com>
2022-04-29 12:30:48 +0530

    Update WebSite.php

--- application/controllers/api/WebSite.php
+++ application/controllers/api/WebSite.php
@@ -61,7 +61,7 @@ class WebSite extends REST_Controller {
         $i=0;
         while ( $i < count($departments) ) { 
             $where = array('department_id' => $departments[$i]['id']);
-            $this->db->order_by('course_type_order,p_order');
+            $this->db->order_by('course_type,course_type_order,p_order');
             $departments[$i]['programs'] = $this->Common_model->get_record('program','*',$where);
             $i++;
         }
