5aaff9331a795ce1ed0f62362eaebf5fb1913312
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2022-07-05 17:10:02 +0530

    Update Admins.php

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -2893,7 +2893,8 @@ public function update_exam_datewise_permission(){
 
 	public function generate_tr($course_group_id="",$class_id=""){
 		$this->db->order_by('roll_number','ASC');
-		$data['students'] = $this->Common_model->getRecordByWhere('student',array("course_group_id"=>$course_group_id ,'class_id' => $class_id ,'new_exam_form'=>'Y','roll_no!='=>'0', 'result_show' => 'N' ));
+		$data['students'] = $this->Common_model->getRecordByWhere('student',array("course_group_id"=>$course_group_id ,'class_id' => $class_id ,'new_exam_form'=>'Y','roll_no!='=>'0'));
+		// , 'result_show' => 'N' 
 		$data['class_id'] = $class_id;
 		$data['course_group_id'] = $course_group_id;
 		$title = "TR ".$this->Common_model->getCourseNameByCourseId($course_group_id).' '.$this->Common_model->getClassNameByClassId($class_id);
