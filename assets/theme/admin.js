2f5de35dcb7ab13133d28512b08add01cbf8502f
Ajay0734 <95852241+Ajay0734@users.noreply.github.com>
2022-07-19 16:19:51 +0530

    done session and mode issue

--- assets/theme/admin.js
+++ assets/theme/admin.js
@@ -1,14 +1,12 @@
 $("#course_group_id").on('change', function(){
 	var csrfName = $('.csrfname').attr('name');
 	var csrfHash = $('.csrfname').val(); 
-	var mode = document.getElementById('mode').value;
 	var course = $(this).val();
 		$.ajax({
 			method: "POST",
 			url: BASE_URL+"admin/Admins/getClassByCourse",
 			data: { course_group_id : course,
 					[csrfName]:csrfHash
-					, mode : mode
 					},
 		})
 		.done(function( msg ) {
