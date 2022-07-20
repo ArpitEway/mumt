2d8d91b88ee803796e2318c8239856497600921f
akshay nagar <akshay.eway@gmail.com>
2022-07-20 18:46:21 +0530

    coming soon exam june

--- application/views/Centers/dashboard.php
+++ application/views/Centers/dashboard.php
@@ -186,12 +186,12 @@
 						<div>
 							<span class="nav-text">Internal Marks Submission</span>
 						</div>
-					</a>
+					</a>-->
 					<a class="border-0 custom-menu-item" href="<?=base_url('exam_form_students');?>">
 						<div>
-							<span class="nav-text">Exam Form Dec 2021</span>
+							<span class="nav-text">Exam Form June 2022</span>
 						</div>
-					</a> -->
+					</a> 
 					<a class="border-0 custom-menu-item" href="<?=base_url('student_roll_no_list');?>">
 						<div>
 							<span class="nav-text">Roll No List</span>
