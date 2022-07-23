ad149f93b8d89b1217b5c177e41c4e0dadb7bf89
neerajeway <106428481+neerajeway@users.noreply.github.com>
2022-07-23 12:32:35 +0530

    add notification at center

--- application/views/Centers/notification.php
+++ application/views/Centers/notification.php
@@ -6,7 +6,9 @@
 		<div class="content-head row mb-3">
 			<div>
 				<ul>
-					<li> Dec 2021 परीक्षा कार्यक्रम - <a href="<?=base_url('assets/instruction/examdate.pdf')?>" target='_blank'>Click Here</a></li>
+					<!-- <li> Dec 2021 परीक्षा कार्यक्रम - <a href="<?=base_url('assets/instruction/examdate.pdf')?>" target='_blank'>Click Here</a></li> -->
+					<li> <img src="<?=base_url('assets/images/center/new.gif')?>" alt="" style="
+					padding: 15px 0px;	"> सूचित किया जाता है कि प्रवेश सम्बन्धी ऑनलाइन प्रक्रिया दिनांक 25 जुलाई 2022 तक अवश्य पूर्ण ले|  इसके पश्चात् किसी भी आवेदन पर विचार नहीं किया जावेगा|  </li>
 				</ul>
 			</div>
 		</div>
