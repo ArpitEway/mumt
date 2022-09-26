1b32b34dc09c749453b88c7401b7f3b482f440da
Ajay0734 <95852241+Ajay0734@users.noreply.github.com>
2022-09-26 18:49:30 +0530

    done changes

--- application/views/admin/student/getStudentConsolidate.php
+++ application/views/admin/student/getStudentConsolidate.php
@@ -14,11 +14,9 @@
 					<th>DOB</th>
 					<th>Course</th>
 					<th>Class</th>
-					<th>
-						<?php if($this->session->account_type == "Admins" || $this->session->account_type == "Enrollment"){ ?>	
-							Edit
-						<?php } ?>
-					</th>
+					<?php if($this->session->account_type == "Admins" || $this->session->account_type == "Enrollment"){ ?>	
+					<th>Edit</th>	
+					<?php } ?>
 					<th>Mobile no</th>
 					<th>Payment</th>
 					<th>Document Uploaded</th>
@@ -81,10 +79,12 @@
 							<td><?php 
 								echo $student["class_name"];
 							 ?></td>
-							<td>
+							
 							<?php if(($this->session->account_type == "Admins" || $this->session->account_type == "Enrollment") && $student['enrolled']!='Y'){ ?> 
+								<td>
 								<a target="_blank"  href='<?php echo base_url($this->session->account_type.'/editForm/').$this->Common_model->encrypt_decrypt($student["student_id"],'encrypt'); ?>'><i class="fa fa-pen"></i></a> 
-							<?php } ?></td>
+								</td>
+							<?php } ?>
 							<td><?php echo $this->Common_model->getMobileNoByStudentID($student["student_id"]) ?></td>
 							<td><?php if( $student["payment_status"]=='Y'){echo 'Paid' ;}else{echo 'Unpaid' ;} ?></td>
 							<td><?php if( $student["document_uploaded"]=='Y'){echo 'Uploaded' ;}else{echo 'Not Uploaded' ;} ?></td>
