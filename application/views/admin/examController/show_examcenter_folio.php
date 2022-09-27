205dd81713d156412be5e8c7539364c49bd44418
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2022-09-27 17:58:00 +0530

    Update show_examcenter_folio.php

--- application/views/admin/examController/show_examcenter_folio.php
+++ application/views/admin/examController/show_examcenter_folio.php
@@ -58,26 +58,26 @@ $words = array('0' => '', '1' => 'One', '2' => 'Two',
                         <tr><td colspan="2" class="tdheight"><h4 align="center"><?php if($two==0) echo '(FOIL)'; else echo "(COUNTERFOIL)"; ?></h4></td></tr><tr></tr>
                         <tr><td colspan="2" class="tdheight"><strong>Name of Exam:</strong>&nbsp;<?php echo $examname;?></td></tr>
                         <tr><td colspan="2" class="tdheight"><strong>Year / Sem:</strong>&nbsp;<?php echo $class_name;?></td></tr>
-                        <tr>
-                            <td style="text-align:left;" colspan="2" class="tdheight"><strong>Exam Session:</strong>&nbsp;<?php echo 'August 2022';?></td></tr>
-                            <tr><td colspan="2" class="tdheight"><strong>Subject : </strong>&nbsp;&nbsp;<?php  echo $paper[0]->paper_name;?></td></tr>
-                            <!-- <tr><td style="text-align:left;" class="tdheight"><strong>Center Code :</strong>&nbsp;<?php echo $exam_center_code;?></td><td class="tdheight"><strong>Q.Paper</strong>&nbsp;<?php echo $paper_code ?></td></tr>-->
-                            <tr><td class="tdheight"><strong>Date of Exam :</strong>&nbsp;<?php echo $edate=date("d-m-Y", strtotime($paper[0]->exam_date));  ?></td><td style="text-align:left;" class="tdheight"><strong>Max.Marks :</strong>&nbsp;<?php echo $paper[0]->max_theory_marks;?></td></tr>
-                            <tr><td class="tdheight"><strong>Examcenter Code:</strong>&nbsp;<?php echo $detail[$examcenter][0]->examcentercode;  ?></td><td style="text-align:left;" class="tdheight"><strong>Q.Paper :</strong>&nbsp;<?php echo $paper[0]->paper_no;?></td></tr>
+                        <tr><td style="text-align:left;" colspan="2" class="tdheight"><strong>Exam Session:</strong>&nbsp;<?php echo 'August 2022';?></td></tr>
+                        <tr><td colspan="2" class="tdheight"><strong>Subject : </strong>&nbsp;&nbsp;<?php  echo $paper[0]->paper_name;?></td></tr>
+                        <!-- <tr><td style="text-align:left;" class="tdheight"><strong>Center Code :</strong>&nbsp;<?php echo $exam_center_code;?></td><td class="tdheight"><strong>Q.Paper</strong>&nbsp;<?php echo $paper_code ?></td></tr>-->
+                        <tr><td class="tdheight"><strong>Date of Exam :</strong>&nbsp;<?php echo $edate=date("d-m-Y", strtotime($paper[0]->exam_date));  ?></td><td style="text-align:left;" class="tdheight"><strong>Max.Marks :</strong>&nbsp;<?php echo $paper[0]->max_theory_marks;?></td></tr>
+                        <tr><td class="tdheight"><strong>Examcenter Code:</strong>&nbsp;<?php echo $detail[$examcenter][0]->examcentercode;  ?></td><td style="text-align:left;" class="tdheight"><strong>Q.Paper :</strong>&nbsp;<?php echo $paper[0]->paper_no;?></td></tr>
                         </table>
                         <table   border="1" class="form-group col-md-12">
                             <tr style="text-align:center;">
                                 <td rowspan="2"  width="77"><strong>En.No. </strong></td>
                                 <td rowspan="2"  width="50"><strong>Roll No.</strong></td>
-                                <td colspan="2"><strong>Marks Awarded</strong></td></tr>
-                                <tr style="text-align:center;">
-                                    <td width="61"><strong>In Fig.</strong></td><td width="344"><strong>In Words</strong></td>
-                                </tr>
-                                <?php 
+                                <td colspan="2"><strong>Marks Awarded</strong></td>
+                            </tr>
+                            <tr style="text-align:center;">
+                                <td width="61"><strong>In Fig.</strong></td><td width="344"><strong>In Words</strong></td>
+                            </tr>
+                            <?php 
                                 foreach($students[$examcenter] as $student){ $rowCounter++;
 
                                   ?> 
-                                  <tr>
+                                <tr>
                                     <td  style="text-align:center;"><?php  echo $student->enrollment_no; ?></td>
                                     <td style="text-align:center;padding: 0px 3px 0px 3px;"><?php echo $student->roll_no;;?></td>
 
@@ -92,13 +92,10 @@ $words = array('0' => '', '1' => 'One', '2' => 'Two',
                                 <table   class="form-group col-md-12">
                                     <tr><td colspan="2" style="text-align:left"><strong>Date :</strong> ____/_____/_________</td></tr>
                                     <tr><td colspan="2" style="text-align:left"><strong>Examiner's Code No :</strong> </td></tr>
-                                    <tr>
-                                        <td style="text-align:left">&nbsp;</td> 
-                                        <td style="text-align:right">&nbsp;</td></tr>
+                                    <tr><td style="text-align:left">&nbsp;</td><td style="text-align:right">&nbsp;</td></tr>
                                         <tr><td colspan="2" style="text-align:right"><strong>(Examiner's Signature)</strong></td></tr>
-                                        <tr><td style="text-align:left;line-height: 20px;"><strong>&nbsp;</strong></td>
-                                            <td style="text-align:right;line-height: 20px;"><?php echo $detail[$examcenter][0]->superintendent; ?></td></tr>
-                                            <tr><td style="text-align:center" colspan="2"><p style="font-size:12px"><strong>Note:Please see instruction being issued separately</strong></p></td></tr>
+                                        <tr><td colspan="2" style="text-align:left;line-height: 20px;"><strong>Full Name :</strong></td></tr>
+                                        <tr><td style="text-align:center" colspan="2"><p style="font-size:12px"><strong>Note:Please see instruction being issued separately</strong></p></td></tr>
                                         </table>
                                         <p class="break"></p>
 
@@ -111,12 +108,11 @@ $words = array('0' => '', '1' => 'One', '2' => 'Two',
                                             <tr><td colspan="2" class="tdheight"><h4 align="center"><?php if($two==0) echo '(FOIL)'; else echo "(COUNTERFOIL)"; ?></h4></td></tr><tr></tr>
                                             <tr><td colspan="2" class="tdheight"><strong>Name of Exam:</strong>&nbsp;<?php echo $examname;?></td></tr>
                                             <tr><td colspan="2" class="tdheight"><strong>Year / Sem:</strong>&nbsp;<?php echo $class_name;?></td></tr>
-                                            <tr>
-                                                <td style="text-align:left;" colspan="2" class="tdheight"><strong>Exam Session:</strong>&nbsp;<?php echo 'August 2022';?></td></tr>
-                                                <tr><td colspan="2" class="tdheight"><strong>Subject : </strong>&nbsp;&nbsp;<?php  echo $paper[0]->paper_name;?></td></tr>
-                                                <!-- <tr><td style="text-align:left;" class="tdheight"><strong>Center Code :</strong>&nbsp;<?php echo $exam_center_code;?></td><td class="tdheight"><strong>Q.Paper</strong>&nbsp;<?php echo $paper_code ?></td></tr>-->
-                                                <tr><td class="tdheight"><strong>Date of Exam :</strong>&nbsp;<?php echo $edate=date("d-m-Y", strtotime($paper[0]->exam_date));  ?></td><td style="text-align:left;" class="tdheight"><strong>Max.Marks :</strong>&nbsp;<?php echo $paper[0]->max_theory_marks;?></td></tr>
-                                                <tr><td class="tdheight"><strong>Examcenter Code:</strong>&nbsp;<?php echo $detail[$examcenter][0]->examcentercode;  ?></td><td style="text-align:left;" class="tdheight"><strong>Q.Paper :</strong>&nbsp;<?php echo $paper[0]->paper_no;?></td></tr>
+                                            <tr><td style="text-align:left;" colspan="2" class="tdheight"><strong>Exam Session:</strong>&nbsp;<?php echo 'August 2022';?></td></tr>
+                                            <tr><td colspan="2" class="tdheight"><strong>Subject : </strong>&nbsp;&nbsp;<?php  echo $paper[0]->paper_name;?></td></tr>
+                                            <!-- <tr><td style="text-align:left;" class="tdheight"><strong>Center Code :</strong>&nbsp;<?php echo $exam_center_code;?></td><td class="tdheight"><strong>Q.Paper</strong>&nbsp;<?php echo $paper_code ?></td></tr>-->
+                                            <tr><td class="tdheight"><strong>Date of Exam :</strong>&nbsp;<?php echo $edate=date("d-m-Y", strtotime($paper[0]->exam_date));  ?></td><td style="text-align:left;" class="tdheight"><strong>Max.Marks :</strong>&nbsp;<?php echo $paper[0]->max_theory_marks;?></td></tr>
+                                            <tr><td class="tdheight"><strong>Examcenter Code:</strong>&nbsp;<?php echo $detail[$examcenter][0]->examcentercode;  ?></td><td style="text-align:left;" class="tdheight"><strong>Q.Paper :</strong>&nbsp;<?php echo $paper[0]->paper_no;?></td></tr>
                                             </table>
                                             <table   border="1" class="form-group col-md-12">
                                                 <tr style="text-align:center;">
@@ -134,16 +130,13 @@ $words = array('0' => '', '1' => 'One', '2' => 'Two',
                                         <table   class="form-group col-md-12">
                                             <tr><td colspan="2" style="text-align:left"><strong>Date :</strong> ____/_____/_________</td></tr>
                                             <tr><td colspan="2" style="text-align:left"><strong>Examiner's Code No :</strong> </td></tr>
-                                            <tr>
-                                                <td style="text-align:left">&nbsp;</td> 
-                                                <td style="text-align:right">&nbsp;</td></tr>
-                                                <tr><td colspan="2" style="text-align:right"><strong>(Examiner's Signature)</strong></td></tr>
-                                                <tr>
-                                                    <td colspan="2" style="text-align:left;line-height: 20px;"><strong>Full Name :</strong></td></tr>
-                                                    <tr><td style="text-align:center" colspan="2"><p style="font-size:12px"><strong>Note:Please see instruction being issued separately</strong></p></td></tr>
-                                                </table>
-                                                <p class="break"></p>
-                                            </div>	
+                                            <tr><td style="text-align:left">&nbsp;</td><td style="text-align:right">&nbsp;</td></tr>
+                                            <tr><td colspan="2" style="text-align:right"><strong>(Examiner's Signature)</strong></td></tr>
+                                            <tr><td colspan="2" style="text-align:left;line-height: 20px;"><strong>Full Name :</strong></td></tr>
+                                            <tr><td style="text-align:center" colspan="2"><p style="font-size:12px"><strong>Note:Please see instruction being issued separately</strong></p></td></tr>
+                                        </table>
+                                            <p class="break"></p>
+                                        </div>	
                                         <?php } ?>
                                     </div>	
                                 <?php } ?>	
