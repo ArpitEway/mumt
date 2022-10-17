c5d1c33f8711450061d85db841b43456e6db592a
akshay nagar <akshay.eway@gmail.com>
2022-10-17 10:24:41 +0530

    Update Otherscript.php

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -56,6 +56,29 @@ class Otherscript extends CI_Controller {
 		}
 	}
 	
+	public function update_extra_papers_in_new_exam()
+	{
+		$where = " paper_code in ('2RMAEDU5', '2RMAGEO6', '2RMAGEO7', '2RMAGEO8', '2RMAPSY5', '2RMSCC7', '2RMSCC8', '2RMSCM7', '2RMSW7')";
+		$papers = $this->Common_model->get_record('paper_master','*',$where);
+
+		foreach ($papers as $paper) {
+			$where = ' class_id = "'.$paper['class_id'].'" and temp_exam_form="Y"';
+			$students = $this->Common_model->get_record('student','*',$where);
+			$studentData = array(
+				'course_group_id' => $paper['course_group_id'],
+				'class_id' => $paper['class_id'],
+				'paper_id' => $paper['id'],
+				'paper_code' => $paper['paper_code'],
+				'paper_type' => $paper['type'],
+				'paper_order' => $paper['paper_no'],		
+			);
+			foreach ($students as $student) {
+				$studentData['student_id'] = $student['student_id'];
+				$this->Common_model->insertAll('new_exam_form',$studentData);
+				echo $this->db->last_query().'<br>';
+			}
+		}	
+	}
 }
 
 ?>
