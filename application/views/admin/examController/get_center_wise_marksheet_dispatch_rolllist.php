3583a199e8c4098c605c032fee1bd732cacc02c6
neerajeway <106428481+neerajeway@users.noreply.github.com>
2022-10-20 15:21:33 +0530

    make changes

--- application/views/admin/examController/get_center_wise_marksheet_dispatch_rolllist.php
+++ application/views/admin/examController/get_center_wise_marksheet_dispatch_rolllist.php
@@ -12,6 +12,7 @@ foreach($centers as $center)  {
 	$this->db->select('*');
 	$this->db->from('student');
 	$this->db->order_by("roll_number", "asc");
+	$this->db->join('course_group', 'student.course_group_id = course_group.id');
 	$where = array('center_id'=>$center->id, 'roll_number!=' => 0 ,'exam_form'=>'Y');
 	$this->db->where($where);	
 	$center_students = $this->db->get()->result();
@@ -23,7 +24,7 @@ foreach($centers as $center)  {
 			$studentCount++;
 			if($sno==0) { ?>
 			<p class="break" style="font-size: 16px;"> </p>
-			<p align="center" style="margin-top:10px;line-height:15px;font-size:15px;"><b>Maharishi Mahesh Yogi Vedic Vishvavidyalaya DDE, Jabalpur</b></p>
+			<p align="center" style="margin-top:10px;line-height:15px;font-size:15px;"><b>Maharishi Mahesh Yogi Vedic Vishvavidyalaya , Jabalpur</b></p>
 			<p align="center" style="line-height:12px;font-size:14px;"><b>Regular List of Roll No. Exam <?=$examTitle?></b></p>
 			<p align="center" style="line-height:12px;font-size:12px;"> <b><i> Center : <?php echo $center->center_code;?> </i></b> </p>
 			<p align="right">Page No :<?php echo $pageno++; ?></p>
@@ -34,11 +35,11 @@ foreach($centers as $center)  {
 					<td style="width:20%;"  valign="top">
 						<table align="center" cellpadding="10" border="1" width="100%">
 							<tr bgcolor="#FFCC99" style="text-align:center;">
-								<th>Roll No. / Enrollment No.</th>
+								<th>Roll No. / Enrollment No./Paper Code</th>
 							</tr>
 						<?php } ?>      
 						<tr style="text-align:center;"> 
-							<td valign="middle">&nbsp;&nbsp;&nbsp;<?php	echo $student->roll_number.' / '.$student->enrollment_no ;?>  </td>
+							<td valign="middle">&nbsp;&nbsp;&nbsp;<?php	echo $student->roll_number.' / '.$student->enrollment_no.' / '.$student->paper_code_pattern ;?>  </td>
 						</tr>
 						<?php if($count==21 ) { ?>
 						</table>
