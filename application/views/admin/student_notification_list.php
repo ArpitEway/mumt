1cf5f44cb8a8bd01a05a4d41570d4e2d85072142
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2022-11-03 17:34:08 +0530

    (One Year Course)

--- application/views/admin/student_notification_list.php
+++ application/views/admin/student_notification_list.php
@@ -61,7 +61,7 @@
 	$notification_no = $this->Common_model->getRecordByWhere('marksheet_variables',array('class_id' => $class_id));
 	$classData = $this->Common_model->getRecordById('class_master','id',$class_id);
 	$isFinalClass = $this->Common_model->hasOneClass($course_group_id);
-	$course_duration = ($isFinalClass) ? "One Year Course" : $classData->class_name;
+	$course_duration = ($isFinalClass) ? "(One Year Course)" : $classData->class_name;
 	$notification=$notification_no[0]->notification_no;
 	$date=$notification_no[0]->result_date;
 	$exam_session=$notification_no[0]->exam_session;
