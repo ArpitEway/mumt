81d3ad2a3d36c4d806e63a4259329d209189dbc4
ShoyebEway <111179622+ShoyebEway@users.noreply.github.com>
2022-12-29 13:46:01 +0530

    Update Dataentry.php

--- application/controllers/admin/Dataentry.php
+++ application/controllers/admin/Dataentry.php
@@ -74,7 +74,7 @@ class Dataentry extends CI_Controller {
 		$this->db->join('student', 'student.student_id = new_exam_form.student_id');
 		$this->db->where('student.class_id = new_exam_form.class_id');
 		$this->db->where($where); 
-		$this->db->limit(10,$page);
+		$this->db->limit(20,$page);
 		$resultData = $this->db->get();
 		$data['resultData'] = $resultData->result();
 
@@ -88,8 +88,8 @@ class Dataentry extends CI_Controller {
 		$this->db->where('student.class_id = new_exam_form.class_id');
 		$this->db->where($where);
 		$config["total_rows"] = $this->db->get()->num_rows();		
-		$config["per_page"] = 10;
-		$config["uri_segment"] = 6;
+		$config["per_page"] = 20;
+		$config["uri_segment"] = 7;
 		$this->pagination->initialize($config);
 		$data["links"] = $this->pagination->create_links();
 		$data['paper_code'] = $paper_code ;
