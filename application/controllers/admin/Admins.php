80b12e09ae5600cef9ad6d5d9a6cc315839b1c32
ShoyebEway <111179622+ShoyebEway@users.noreply.github.com>
2023-02-20 18:15:37 +0530

    Update Admins.php

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -4599,7 +4599,7 @@ public function update_exam_datewise_permission(){
 			$set = array_column($classes,'id');
 				$this->db->select('*');
 				$this->db->from('new_exam_form');
-				$this->db->join('student', 'new_exam_form.student_id = student.student_id');
+				$this->db->join('student', 'new_exam_form.student_id = student.student_id and new_exam_form.class_id = student.class_id');
 				$this->db->where('student.exam_form','Y');
 				if($mode != 'All'){
 					$this->db->where_in('new_exam_form.class_id',$set);
