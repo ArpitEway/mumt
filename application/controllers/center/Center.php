1e3f5e4afd740573993415a0d7b451519639f2a9
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2023-03-14 12:11:28 +0530

    Update Center.php

--- application/controllers/center/Center.php
+++ application/controllers/center/Center.php
@@ -167,7 +167,7 @@ class Center extends CI_Controller {
 		$check = $this->Common_model->getRecordByWhere("center",$where);
 		
 		$center_id =  $this->session->center_id;
-		$center_ids_dep = array(10,11,12,21,22,23,24,25,26,27,28,29);
+		$center_ids_dep = array(10,11,12,21,22,23,24,25,26,27,28,29,2115);
 
 		if(($mode=='regular' && $check[0]->admission_permission!='Y' && !in_array($center_id, $center_ids_dep)) || ($mode=='private' && $check[0]->admission_permission_private!='Y')){
 			redirect(base_url('dashboard'));
