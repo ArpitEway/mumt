5a5772d0a6bc60b2813df27438128f6699c9ef18
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2023-06-02 11:49:16 +0530

    update

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -3035,8 +3035,8 @@ public function update_exam_datewise_permission(){
 	}
 
 	public function tr_class_list(){
-		$where = "id in (select distinct(course_group_id) from student_result_aug_22 where exam_form = 'Y' )";
-		// new_exam_form = 'Y' or
+		$where = "id in (select distinct(course_group_id) from student where exam_form = 'Y' )";
+		// new_exam_form = 'Y' or student_result_aug_22
 		// and class_id in (104,107,134,283,285,287,289,293,295,297,291)
 		$data['courses'] = $this->Common_model->get_record('course_group','*',$where);
 		$this->load->view('header',array('title' => 'Class List'));
