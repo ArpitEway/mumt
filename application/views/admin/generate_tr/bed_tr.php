7110d0b0d7d9817d10ebb1cb02fbb7b2186b093e
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2023-06-02 12:01:09 +0530

    Update bed_tr.php

--- application/views/admin/generate_tr/bed_tr.php
+++ application/views/admin/generate_tr/bed_tr.php
@@ -351,7 +351,9 @@
       <tbody>
         <tr>
           <th  class="align-middle text-center roll_no" rowspan="<?php echo $rowspandata ?>"><?php  echo $student->roll_number ?> <br> <?php echo $student->enrollment_no  ?></th>
-          <th class="align-middle text-center ms_no" rowspan="<?php echo $rowspandata ?>"><?=$student->marksheet_no ?></th>
+          <th class="align-middle text-center ms_no" rowspan="<?php echo $rowspandata ?>">
+            <!-- <?= $student->marksheet_no ?> -->
+          </th>
           <th  class="align-middle text-center photo" rowspan="<?php echo $rowspandata ?>">
             <img alt="N/A" src="<?= base_url('assets/student_image/'.$student->session.'/'.$student->photo) ?>" width='65px' height="90px"></th>
           <td  class="align-middle text-center name"  rowspan="<?php  echo $rowspandata ?>"><?php  echo $student->name ?>/ <br><?php  echo $student->f_h_name ?></td>
