771d5f14a2af5c61263f469f200f470f7542cfd0
ShoyebEway <111179622+ShoyebEway@users.noreply.github.com>
2023-06-26 12:47:42 +0530

    Update Permission.php

--- application/controllers/admin/Permission.php
+++ application/controllers/admin/Permission.php
@@ -18,7 +18,8 @@ class Permission extends CI_Controller {
        
 		$this->load->view('header',array("title"=>"Class Wise Permission"));	
 		$table ="class_master";
-		$where = 'admission_permission="Y" or class_name="II Sem" or class_name="III Sem"';
+		$where = 'admission_permission="Y"';
+		// or class_name="II Sem" or class_name="III Sem"
 		 // $order = 'id ASC';
         $data['name_csrf'] = $this->security->get_csrf_token_name();
 		$data['hash_csrf'] = $this->security->get_csrf_hash();
