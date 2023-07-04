5af2e1d4b9fb1c6f814cc95c8f8d5b2e3a29e53a
neerajeway <106428481+neerajeway@users.noreply.github.com>
2023-07-04 10:12:45 +0530

    work on group exam paper add

--- application/controllers/admin/scripts/Preexam.php
+++ application/controllers/admin/scripts/Preexam.php
@@ -328,6 +328,74 @@ class Preexam extends CI_Controller {
 	// 	}
 
 	// }
+
+
+	public function upload_group_exam_paper(){
+		$this->load->view('admin/script/header',array('title' => 'Upload Exam Paper'));
+		/* class which dose not have elective papers */
+		
+		//$classes = $this->Common_model->get_record('class_master','GROUP_CONCAT(id) as class_id',array('class_group' => 'N'));
+		//,'exam_form_permission' => 'Y','admission_permission' => 'Y'
+		//$class_ids = $classes[0]['class_id'];
+		$class_ids =104;
+		$this->db->select('count(class_id) as num,course_name,class_name,class_id');
+		$this->db->where('class_id in ('.$class_ids.') and temp_exam_form="N" and new_exam_form="N" and demo="N"');// and payment_status="Y"
+		$this->db->group_by('class_id');
+		$this->db->order_by('course_group_id');
+		$studentClasses = $this->db->get('student')->result();
+		$i=0;
+		
+		
+		
+		// $this->Common_model->last_query();
+		$data = array(
+			'studentClasses' => $studentClasses,
+		);
+		$this->load->view('admin/script/upload_group_exam_paper',$data);
+		$this->load->view('admin/script/footer');
+	}
+
+	public function upload_group_exam_paper_sub($class_id)
+	{
+		$where = array('class_id' => $class_id,
+					//'payment_status' => 'Y',
+					'temp_exam_form' => "N",
+					'new_exam_form'=>"N" ,
+					'demo'=>"N",
+		);
+			
+		$students = $this->Common_model->get_record('student','*',$where);
+		
+		$paperWhere=array('class_id'=>$class_id,'ce'=>'compulsory');
+		$papers = $this->Common_model->get_record('paper_master','*',$paperWhere);
+		foreach ($students as $student) {
+			$where = array('student_id'=>$student['student_id']);
+			$data = array(
+				'student_id' => $student['student_id'],
+				'course_group_id' => $student['course_group_id'],
+				'class_id' => $student['class_id'],
+				);
+			
+			foreach ($papers as $paper) {
+				
+				$data['paper_id'] = $paper['id'];
+				$data['paper_code'] = $paper['paper_code'];
+				$data['paper_type'] = $paper['type'];
+				$data['paper_order'] = $paper['paper_no'];
+				$data['sub_group_id'] = $paper['sub_group_id'];
+				$this->Common_model->insertAll('new_exam_form',$data);
+    			echo $this->db->last_query().'<br>';
+				
+				
+			}
+			
+				//$electiveWhere=array('class_id'=>$class_id);
+				//$elective = $this->Common_model->get_record('paper_master','*',$electiveWhere);
+			//SELECT * FROM `group` WHERE `class_id`=105 and `group_name`='Group A (History, Political Science, Sociology)';
+			$this->Common_model->updateRecordByConditions('student',$where,array('temp_exam_form' => "Y"));
+		}
+	}
+
 }
 
 ?>
