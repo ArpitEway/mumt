7fb8e3ea0767faac28e30571c09d89e399cd120e
neerajeway <106428481+neerajeway@users.noreply.github.com>
2023-07-15 15:31:43 +0530

    test

--- application/views/admin/paper_test_id_list.php
+++ application/views/admin/paper_test_id_list.php
@@ -19,9 +19,11 @@
 		<?php $i=1; ?>
 		<?php foreach ($papers as $paper): 
 			$old_paper_master ="";
-			$old_paper_master = $this->Common_model->getRecordByWhere('paper_master_aug_2022',array('id'=>$paper->id));
-			
-			//echo $old_paper_master[0]->exam_date; die;
+			$old_paper_master = $this->Common_model->getRecordByWhere('paper_master_aug_2022',array('id'=>$paper->id));//('id'=>1116));
+			//
+			// echo $this->db->last_query().'<br>';
+			// print_r( $old_paper_master[0]);
+			// echo $old_paper_master[0]->exam_date; die;
 			?>
 			<tr>
 				<td><?=$i++ ?></td>
