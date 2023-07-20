0f88fbcadc9c16654f88fa500c52b0e050d2438d
neerajeway <106428481+neerajeway@users.noreply.github.com>
2023-07-20 14:51:49 +0530

    marksheet date change

--- application/views/admin/msprint/view_student_result.php
+++ application/views/admin/msprint/view_student_result.php
@@ -147,6 +147,9 @@
                   <div class="col-md-1">
                       <label class="text-heading">View </label>
                   </div>
+                  <div class="col-md-1">
+                      <label class="text-heading">Edit Date</label>
+                  </div>    
                
               </div>
               <?php 
@@ -185,6 +188,13 @@
                          <!-- <label class="text-heading mt-3"><a class="  btn-sm" href="<?= base_url('MsPrint/marksheet/'.$id.'')?>" target="_blank"><i class="fa fa-eye"></i></a></label> -->
                          <label class="text-heading mt-3"><a href="<?= base_url( $this->session->account_type.'/marksheet/'.$id.'')?>" target="_blank"><i class="fa fa-eye"></i></a></label>
                     </div>
+
+                    <div class="col-md-1">  
+                         <label class="text-heading mt-3">
+                              <a href="#"  class=" font-weight-bold marksheet_update" data-toggle="modal" data-student_name = "<?=$student->name ?>" data-record_id="<?php echo $id ?>"
+                              data-roll_number = "<?= $res->roll_no; ?>" data-marksheet_date = "<?php echo date('d/m/Y', strtotime($res->marksheet_date)); ?>"  data-target="#kt_datepicker_modal" "  ><i class="fa fa-edit"></i></a>
+                        </label>
+                     </div>
                         
                  </div>
              <?php } ?>
@@ -192,6 +202,58 @@
  	</div>
 </div>
 
+
+				 
+<div class="modal fade" id="kt_datepicker_modal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-md" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Marksheet Details</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<i aria-hidden="true" class="ki ki-close"></i>
+				</button>
+			</div>
+			<div class="card card-custom">
+
+            <!--begin::Form-->
+            <form method="POST" class="d-block" id="ajaxForm" action="<?php echo site_url('admin/MsPrint/update_marksheet_date'); ?>">
+            <div class="card-body">
+            <input type="hidden" class="csrfname" name="<?= $name_csrf; ?>" value="<?= $hash_csrf; ?>">
+           
+            <div class="form-group row">
+                <label for="example-date-input" class="col-5 col-form-label">Student Name</label>
+                <div class="col-7">
+                <label for="example-date-input" class="col-form-label"><span id="student_name"></span></label>
+                </div>
+                
+            </div>
+            <div class="form-group row">
+                <label for="example-date-input" class="col-5 col-form-label">Roll Number</label>
+                <div class="col-7">
+                <label for="example-date-input" class="col-form-label"><span id="roll_number"></span></label>
+                </div>
+                
+            </div>
+            <div class="form-group row">
+                <label for="example-date-input" class="col-5 col-form-label">Marksheet Date</label>
+                <div class="col-7">
+                    
+                <input class="form-control" type="date" name="marksheet_date"   id="marksheet_date"  />
+                <div class="text-danger" id="error"></div>
+                <input type="hidden" value="" name="record_id" id="record_id">
+              
+                </div>
+            </div>
+           <div class="card-footer pb-0">
+            <div class="row justify-content-center">
+            
+                <button type="reset" class="btn btn-success mr-2" id="date_submit">Submit</button>
+                
+            
+            </div>
+            </div>
+            </form>
+</div>
 <script>
     $(document).on('click','.marksheet_update',function(){
 	    var name_csrf = $(this).attr('data-name_csrf');
@@ -207,4 +269,46 @@
 		
 	});
 
+    $("#date_submit").on('click',function (e){
+	var formimage = $('#ajaxForm');
+	var frm = new FormData(formimage[0]);
+		
+        var record_id = $('#record_id').val();
+		var marksheet_date = $('#marksheet_date').val();
+		var roll_number = $('#roll_number').html();
+		if(marksheet_date==''){
+			$('#error').text('Please Select Date');
+			return false;
+		}
+				
+		$.ajax({
+		url: '<?php echo site_url('MsPrint/update_marksheet_date'); ?>',
+		type: 'POST',
+		dataType : 'json',
+		data: frm,
+		cache:false,
+		contentType: false,
+		processData: false,
+		beforsend: function()
+              {
+                console.log('loading..');
+                $("#myLoader").show();
+               },
+		success: function (data) {
+		if(data){
+			console.log(data);
+			$('#kt_datepicker_modal').modal('toggle');
+				toastr.success("Date Updated Successfully!");
+		}else{
+			toastr.error("Something wrong");
+		}
+			},
+			complete: function()
+              {
+                console.log('loading...over');
+                $("#myLoader").hide();
+               },
+		});	
+	
+});	
 </script>    
