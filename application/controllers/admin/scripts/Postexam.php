e923c63cc904d0509feb520814ed57fb72d32971
neerajeway <106428481+neerajeway@users.noreply.github.com>
2023-07-21 15:38:02 +0530

    add condition

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -373,6 +373,7 @@ class Postexam extends CI_Controller {
         $this->db->from('class_master');
         $this->db->where('class_name','II Sem');
         //$this->db->where('backlog_exam_form_permission','Y');
+        $this->db->where('old_exam_form_permission','N');
         $classes = $this->db->get()->result();
        $class_id = array_column($classes,'id');
        if($classes){
