cbc1903617526be48e9d56b520b9220668b0418b
ShoyebEway <111179622+ShoyebEway@users.noreply.github.com>
2023-07-28 15:38:31 +0530

    Update Admins.php

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -4955,11 +4955,12 @@ public function update_exam_datewise_permission(){
 				// 	$this->db->where_in('new_exam_form.class_id',$set);
 				// }
 				$this->db->where_in('new_exam_form.class_id',$set);
-				// $this->db->where('new_exam_form.theory_marks','');
+				 $this->db->where('new_exam_form.theory_marks','');
 				$this->db->where('new_exam_form.paper_type',"theory");
 				$this->db->order_by('student.course_group_id','student.old_class_id','student.university_mode','student.roll_number');
 				
 			$data['students'] = $this->db->get()->result();
+			
 			$dt = $this->load->view('admin/class_wise_remaining_report_table_old',$data,true);
 		
 			echo json_encode(array(
