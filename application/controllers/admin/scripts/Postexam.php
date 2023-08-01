71c985f8df54dc2557b5a9e425a8f68cfc5e7f48
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2023-08-01 18:49:14 +0530

    Update Postexam.php

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -374,9 +374,9 @@ class Postexam extends CI_Controller {
         $this->db->select('DISTINCT(id)');
         $this->db->from('class_master');
         // $this->db->where('class_name','I Year');
-        //$this->db->where('backlog_exam_form_permission','Y');
+        $this->db->where('backlog_exam_form_permission','Y');
         //$this->db->where('old_exam_form_permission','N');
-         $this->db->where('exam_form_permission','Y');
+         // $this->db->where('exam_form_permission','Y');
         $classes = $this->db->get()->result();
        $class_id = array_column($classes,'id');
        if($classes){
