210f23d3d3b09d3ab8f34eb109a2148f448216fa
neerajeway <106428481+neerajeway@users.noreply.github.com>
2023-08-23 16:05:54 +0530

    Update MsPrint.php

--- application/controllers/admin/MsPrint.php
+++ application/controllers/admin/MsPrint.php
@@ -172,7 +172,7 @@ class MsPrint extends CI_Controller {
 			$this->db->where_in('id',$ids);
 		$this->db->order_by('center_code', "asc");
 		$data['centers'] = $this->db->get()->result();
-		$data['examTitle'] = "March 2022";
+		$data['examTitle'] = "March 2023";
 		
 		echo $this->load->view('admin/examController/get_center_wise_marksheet_dispatchlist',$data, TRUE);
 	}
