2c4f933cee2aaa9ca4f216674a0511218f6218c2
neerajeway <106428481+neerajeway@users.noreply.github.com>
2023-09-15 15:28:05 +0530

    Update Otherscript.php

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -538,7 +538,7 @@ public function update_roll_no_old_data(){
 		 $class_id=227;
 		$num_of_papers=6;
 		
-		$sql="SELECT count(*)as num,e.* FROM `old_result_data` as e join `old_exam_data` as s on s.id=e.`exam_data_id` WHERE s.`class_id`=".$class_id." AND exam_year='Feb 2023' and exam_status='R' group by s.student_id HAVING num='".$num_of_papers."'";
+		$sql="SELECT count(*)as num,e.* FROM `old_result_data` as e join `old_exam_data` as s on s.id=e.`exam_data_id` WHERE s.`class_id`=".$class_id." AND exam_year='March 2023' and exam_status='R' group by s.student_id HAVING num='".$num_of_papers."'";
 		$sql_result = $this->db->query($sql);
         $students = $sql_result->result_array();
 		foreach ($students as $student) {
