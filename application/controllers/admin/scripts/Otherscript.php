14845932cddb14f05d83895313f4eb45087f94a8
neerajeway <106428481+neerajeway@users.noreply.github.com>
2023-11-27 12:23:44 +0530

    make change for internal

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -156,11 +156,12 @@ class Otherscript extends CI_Controller {
 
 	public function update_int_marks($cls_id =0)
 	{
-		 $marks = array('18','17','16','18','17','16','15','15');
+		$marks = array('18','17','16','18','17','16','15','15');
+		// $marks = array('18','17','16','18','17','16','15','15');
 		// $marks = array('09','08','07','09','08','07','06','06');
 		//$marks = array('27','26','25','27','26','25','24','24'); 
-		$cls_id=182;
-		$sql = "select * from student where class_id='".$cls_id."' and exam_form='Y' and int_marks_sub='N' and roll_number!=0 and university_mode='REG' limit 100";
+		$cls_id=248;
+		$sql = "select * from student where class_id='".$cls_id."' and exam_form='Y' and int_marks_sub='N' and roll_no!=0 and university_mode='REG' limit 100";
 		$rs = $this->db->query($sql)->result_array();
 		$s_no=1;
 		foreach ($rs as $student) {
