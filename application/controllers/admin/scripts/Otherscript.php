5e80f256ffb884cc25757ee2b6e250a137ba88c5
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2023-12-11 13:03:35 +0530

    Add Bachelor of Physical Education Paper in old

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -537,10 +537,10 @@ public function update_roll_no_old_data(){
 	{
 		
 		//$class_id=253;
-		 $class_id=216;
+		 $class_id=269;
 		$num_of_papers=8;
 		
-		$sql="SELECT count(*)as num,e.* FROM `old_result_data` as e join `old_exam_data` as s on s.id=e.`exam_data_id` WHERE s.`class_id`=".$class_id." AND exam_year='July 2023' and exam_status='R' group by s.student_id HAVING num='".$num_of_papers."'";
+		$sql="SELECT count(*)as num,e.* FROM `old_result_data` as e join `old_exam_data` as s on s.id=e.`exam_data_id` WHERE s.`class_id`=".$class_id." AND exam_year='May 2023' and exam_status='R' group by s.student_id HAVING num='".$num_of_papers."'";
 		$sql_result = $this->db->query($sql);
         $students = $sql_result->result_array();
 		foreach ($students as $student) {
