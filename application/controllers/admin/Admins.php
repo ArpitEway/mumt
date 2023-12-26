80c6514f5d493556c6973f6bb1720453fb109a70
neerajeway <106428481+neerajeway@users.noreply.github.com>
2023-12-26 16:22:51 +0530

    Update Admins.php

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -3709,10 +3709,15 @@ public function update_exam_datewise_permission(){
 		));
 	}
 
-	public function generate_tr_bed($mode="",$course_group_id="",$class_id=""){
+	public function generate_tr_bed($mode="",$exam_pattern="M",$course_group_id="",$class_id=""){
 		$this->db->order_by('center_id,roll_number','ASC');
-		
-		$data['students'] = $this->Common_model->getRecordByWhere('student',array("university_mode"=>$mode,"course_group_id"=>$course_group_id ,'class_id' => $class_id ,'new_exam_form'=>'Y','roll_no!='=>'0' ));
+		if($exam_pattern=="M"){
+			$pattern="MARKS";
+		}
+		else{
+			$pattern="GRADE";
+		}
+		$data['students'] = $this->Common_model->getRecordByWhere('student',array("university_mode"=>$mode,"course_group_id"=>$course_group_id ,'class_id' => $class_id ,'new_exam_form'=>'Y','roll_no!='=>'0' ,'exam_pattern'=> $pattern));
 
 		// $data['students'] = $this->Common_model->getRecordByWhere('student_result_aug_22',array("university_mode"=>$mode,"course_group_id"=>$course_group_id ,'old_class_id' => $class_id ,'exam_form'=>'Y','roll_number!='=>'0' ));
 		//'result_show' => 'N' ,'student_id'=>'685381'
