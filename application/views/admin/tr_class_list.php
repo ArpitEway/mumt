b549e3fdf7fc1b7fb4f799a1ae3234996dccbcef
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-01-01 17:34:01 +0530

    add class

--- application/views/admin/tr_class_list.php
+++ application/views/admin/tr_class_list.php
@@ -11,7 +11,7 @@
 		<?php foreach ($courses as $course){
 			$this->db->order_by('id');
 			
-			 $this->db->where_in('id',array(214,200,210,222,198,202,212,224,226,208,218,162,172,128,129));
+			 $this->db->where_in('id',array(206,228,230,242,299,126,132));
 			
         $classes= $this->Common_model->getRecordByWhere('class_master',array("course_group_id"=>$course['id'] , 'exam_form_permission' => 'Y'));
 		//, 'old_exam_form_permission' => 'Y'
