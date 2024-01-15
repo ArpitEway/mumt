6d48fb8c16914a7b57ff293294b603763a426326
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-01-15 13:06:21 +0530

    link on

--- application/controllers/admin/ExamController.php
+++ application/controllers/admin/ExamController.php
@@ -3117,7 +3117,7 @@ public function getStudentData()
 	  }
 	}
 	public function search_student_result_for_wh(){
-		redirect(base_url().'ExamController/');
+		//redirect(base_url().'ExamController/');
 		$data['name_csrf'] = $this->security->get_csrf_token_name();
 		$data['hash_csrf'] = $this->security->get_csrf_hash();
 		$this->load->view('header',array('title' => 'Edit Student Marks'));	
@@ -3128,7 +3128,7 @@ public function getStudentData()
 	public function getEditStudentMarksDataWH()
 	{
 		$roll_no = $this->input->post('roll_no');
-		$studentData = $this->Common_model->getRecordByWhere('student',array('roll_number'=>$roll_no,'exam_form'=>'Y'));
+		$studentData = $this->Common_model->getRecordByWhere('student',array('roll_no'=>$roll_no,'new_exam_form'=>'Y'));
 		$studentPaper = $this->Common_model->get_student_papers($studentData[0]->student_id,$studentData[0]->class_id);
 		$data['student'] = $studentData;
 		$data['wh'] = true;
