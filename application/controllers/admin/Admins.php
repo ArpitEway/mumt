8f9c4e1849e079aa8a32fcdf5a63f34490e649ef
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2024-01-18 15:34:35 +0530

    add class id

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -3190,7 +3190,7 @@ public function update_exam_datewise_permission(){
 	}
 
 	public function tr_class_list(){
-		$where = "id in (select distinct(course_group_id) from student where new_exam_form = 'Y' and class_id in (228,126,108,111,117,285,262,293,295,107,125,298))";
+		$where = "id in (select distinct(course_group_id) from student where new_exam_form = 'Y' and class_id in (228,126,108,111,117,285,262,293,295,107,125,298,105))";
 		/******Final TR*******/
 		//206,228,230,242,299,126,132
 		// 293,295,183,300,268,256,258,289,283,291,174,191,177,194,196,204,276,280,120,287,297,101,102,119
--- application/views/admin/tr_class_list.php
+++ application/views/admin/tr_class_list.php
@@ -11,7 +11,7 @@
 		<?php foreach ($courses as $course){
 			$this->db->order_by('id');
 			
-			 $this->db->where_in('id',array(228,126,108,111,117,285,262,293,295,107,125,298));
+			 $this->db->where_in('id',array(228,126,108,111,117,285,262,293,295,107,125,298,105));
 			
         $classes= $this->Common_model->getRecordByWhere('class_master',array("course_group_id"=>$course['id'] , 'exam_form_permission' => 'Y'));
 		//, 'old_exam_form_permission' => 'Y'
