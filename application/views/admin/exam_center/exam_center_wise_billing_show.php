b048f9128ca65c71ae2e606ce45cfc7e1dfcc88b
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-03-01 17:23:17 +0530

    Update exam_center_wise_billing_show.php

--- application/views/admin/exam_center/exam_center_wise_billing_show.php
+++ application/views/admin/exam_center/exam_center_wise_billing_show.php
@@ -127,9 +127,9 @@ foreach($exam_centers as $row)
             $this->db->where_not_in('paper_no_for_time_table', array('1B','2B'));
             $papers_all = $this->Common_model->get_record('paper_master','*',$where);
             $papersid = array_column($papers_all, 'id');
-            echo "<br>";
-            echo $this->db->last_query(); 
-            echo "<br>";
+            // echo "<br>";
+            // echo $this->db->last_query(); 
+            // echo "<br>";
             $this->db->select('count(*) as cnt');
             $this->db->from('student as s');
             $this->db->join('new_exam_form  as e', 'e.student_id = s.student_id AND s.class_id = e.class_id');
