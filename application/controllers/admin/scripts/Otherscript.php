646a6aff792ffe5d2c973127d8ace05257112ee0
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-04-17 15:10:50 +0530

    Update Otherscript.php

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -61,7 +61,7 @@ class Otherscript extends CI_Controller {
 		//$where = " paper_code in ('1RMLIS10','1RMLIS11')";
 		//$where="`paper_code` in ('1RCMSCCH7','1RCMSCCH8','1RCMSCCH9','1RCMSCBT7','1RCMSCBT8','1RCMSCC7','1RCMSCM7','3RMSCC6','3RMSCC7')";
 	//	$where="`id` in (1461)";
-		$where="`id` >= 1461";
+		$where="`id` = 0";
 		$papers = $this->Common_model->get_record('paper_master','*',$where);
 
 		foreach ($papers as $paper) {
