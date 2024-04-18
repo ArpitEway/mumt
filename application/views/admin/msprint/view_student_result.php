7b55bdec3304899b3b59421fd22ea1db998e3ff5
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-04-18 15:44:29 +0530

    Update view_student_result.php

--- application/views/admin/msprint/view_student_result.php
+++ application/views/admin/msprint/view_student_result.php
@@ -184,7 +184,7 @@
                     <?php 
                     
                     if($res->exam_status == "B"){
-                        if(($res->exam_year=="July 2023") && (in_array($res->class_id, $grade_classes))){
+                        if(($res->exam_year=="July 2023") && $res->marks_pattern=="GRADE" && (in_array($res->class_id, $grade_classes))){
                             $backlogflag=false;
                            
                         }
