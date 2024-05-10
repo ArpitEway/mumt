fc4aabb7ca58c8ac976f7c925e23cb7697fe9b04
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-05-10 18:19:03 +0530

    Update Preexam.php

--- application/controllers/admin/scripts/Preexam.php
+++ application/controllers/admin/scripts/Preexam.php
@@ -19,7 +19,7 @@ class Preexam extends CI_Controller {
 		/* class which dose not have elective papers */
 		
 		$classes = $this->Common_model->get_record('class_master','GROUP_CONCAT(id) as class_id',array('class_group' => 'N'));
-		//,'exam_form_permission' => 'Y','admission_permission' => 'Y'
+		//,'exam_form_permission' => 'Y','exam_form_permission' => 'Y','admission_permission' => 'Y'
 		$class_ids = $classes[0]['class_id'];
 		
 		$this->db->select('count(class_id) as num,course_name,class_name,class_id');
@@ -350,10 +350,10 @@ class Preexam extends CI_Controller {
 		$this->load->view('admin/script/header',array('title' => 'Upload Exam Paper'));
 		/* class which dose not have elective papers */
 		
-		//$classes = $this->Common_model->get_record('class_master','GROUP_CONCAT(id) as class_id',array('class_group' => 'N'));
+		$classes = $this->Common_model->get_record('class_master','GROUP_CONCAT(id) as class_id',array('class_group' => 'Y'));
 		//,'exam_form_permission' => 'Y','admission_permission' => 'Y'
-		//$class_ids = $classes[0]['class_id'];
-		$class_ids =216; //105;
+		$class_ids = $classes[0]['class_id'];
+		//$class_ids =216; //105;
 		$this->db->select('count(class_id) as num,course_name,class_name,class_id');
 		$this->db->where('class_id in ('.$class_ids.') and temp_exam_form="N" and demo="N"');
 		// and payment_status="Y" // and new_exam_form="N"
