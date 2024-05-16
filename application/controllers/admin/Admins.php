848dc0e7cdc0b56963cd675a1d9f051c13fe4ec7
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-05-16 18:39:09 +0530

    set old model

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -5663,7 +5663,7 @@ public function forward_complaint(){
 			$this->load->model('Gradesheet_old_model');
 			$this->load->view('admin/grade_marksheet',$data);
 		}else if($data['class']->cbcs=='Y' && $data['exam_data']->university_mode=='REG' && $data['exam_data']->marks_pattern=='GRADE'){
-				$this->load->model('Gradesheet_model_pg');
+				$this->load->model('GradeSheet_old_model_pg');
 				$this->load->view('admin/grade_marksheet_pg',$data);
 		}else if($data['exam_data']->university_mode !="PVT"  && $data['class']->internal !='N'){
 			
