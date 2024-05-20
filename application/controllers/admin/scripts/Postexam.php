5145d50dec6895096bd326a8d0269f739dddf1f0
ShoyebEway <111179622+ShoyebEway@users.noreply.github.com>
2024-05-20 16:15:54 +0530

    update script

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -580,11 +580,11 @@ class Postexam extends CI_Controller {
          } 
      }
 
-     public function backlog_marks_add_scripts($student_id,$class_id='',$exam_year)
+     public function backlog_marks_add_scripts($student_id,$class_id,$exam_year,$id)
      {
        
         $exam_year = str_replace("%20"," ",$exam_year);
-         $students = $this->Common_model->getRecordByWhere("old_exam_data",array("class_id"=>$class_id,'student_id'=>$student_id,'exam_year'=>$exam_year));
+         $students = $this->Common_model->getRecordByWhere("old_exam_data",array("class_id"=>$class_id,'student_id'=>$student_id,'exam_year'=>$exam_year,'id'=>$id));
         
          $whereResult = array("class_id"=>$students[0]->class_id ,"student_id"=>$students[0]->student_id, 'exam_data_id' => $students[0]->id);
          $old_result_datas = $this->Common_model->getRecordByWhere("old_result_data",$whereResult );
@@ -595,7 +595,7 @@ class Postexam extends CI_Controller {
                  'roll_no' => 0,
                  'session' => $students[0]->session,
                  'mode'=>$students[0]->university_mode,
-                 'exam_year'=>'Dec 2023',
+                 'exam_year'=>'June 2024',
                  'exam_form' => 'N',
                  'enrollment_no' => $students[0]->enrollment_no,
                  'center_id' => $students[0]->center_id,
@@ -607,7 +607,7 @@ class Postexam extends CI_Controller {
                  'upload_result' =>  'N',
                  'result_permission' => 'N',
                 );
-               $duplicate =  $this->Common_model->getRecordByWhere('backlog_student',array('student_id'=>$students[0]->student_id,'class_id'=>$students[0]->class_id,'exam_year'=>'Dec 2023'));
+               $duplicate =  $this->Common_model->getRecordByWhere('backlog_student',array('student_id'=>$students[0]->student_id,'class_id'=>$students[0]->class_id,'exam_year'=>'June 2024'));
              if( $duplicate !== Array ( )){
                  echo "Already Exist";
                }else{
--- application/views/admin/msprint/view_student_result.php
+++ application/views/admin/msprint/view_student_result.php
@@ -187,7 +187,7 @@
                  <div class="col-md-2">
                     <label class="text-heading mt-3"><?= $res->exam_result;?></label>
                     <?php if($res->exam_result == 'FAIL' && $this->session->account_type == 'Admins' ){?>
-                        <label class="text-heading mt-3"><a href="<?= base_url( 'admin/scripts/Postexam/backlog_marks_add_scripts/'.$student->student_id.'/'.$res->class_id.'/'.$res->exam_year.'')?>" target="_blank"><i class="fa fa-plus"></i></a></label>
+                        <label class="text-heading mt-3"><a href="<?= base_url( 'admin/scripts/Postexam/backlog_marks_add_scripts/'.$student->student_id.'/'.$res->class_id.'/'.$res->exam_year.'/'.$res->id.'')?>" target="_blank"><i class="fa fa-plus"></i></a></label>
                         <?php } ?>    
                  </div> 
               <div class="col-md-2 text-heading mt-3">
