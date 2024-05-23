3226091a79f54857736ab398f109ec9b4497b2d1
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2024-05-23 18:56:50 +0530

    Update class_wise_remaining_report_table_old.php

--- application/views/admin/class_wise_remaining_report_table_old.php
+++ application/views/admin/class_wise_remaining_report_table_old.php
@@ -35,9 +35,10 @@
                 <th>Class</th>
                 <!-- <th>Session</th> -->
                 <th>Paper Code</th>
+                <!-- 
                 <?php if($this->session->account_type == "ExamController"){ ?>
                 <th>Action</th>
-                <?php } ?>
+                <?php } ?>-->
                
                
             </tr>
@@ -58,9 +59,11 @@
                 <td><?= $this->Common_model->getClassNameByClassId($student->old_class_id) ?></td>
                 <!-- <td><?= $student->session ?></td> -->
                 <td><?= $student->paper_code ?></td>
+                 <!-- 
                 <?php if($this->session->account_type == "ExamController"){ ?>
                 <td><a target="_blank" href="<?php echo  base_url()."ExamController/search_student_result/$student->roll_number" ?>">Edit </a></td>
                 <?php } ?>
+                -->
               
             </tr>
             <?php  }  ?>
