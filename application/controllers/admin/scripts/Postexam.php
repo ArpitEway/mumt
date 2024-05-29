7bfda9588a770f118e8e054940f25be2281f9e01
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-05-29 17:04:41 +0530

    Update Postexam.php

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -474,7 +474,7 @@ class Postexam extends CI_Controller {
         // $this->db->where('class_name','I Year');
       //  $this->db->where_in('class_name',array('II SEM','IV SEM'));
       //  $this->db->where('backlog_exam_form_permission','Y');
-        $this->db->where('old_exam_form_permission','N');
+        $this->db->where('old_exam_form_permission','Y');
         $this->db->where('exam_form_permission','Y');
         $classes = $this->db->get()->result();
         $class_id = array_column($classes,'id');
