7268582f5ad590f5be9216ddb0e8ecc0d7a06ac7
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-06-15 18:33:42 +0530

    Update exam_center_paper_count_show.php

--- application/views/admin/exam_center/exam_center_paper_count_show.php
+++ application/views/admin/exam_center/exam_center_paper_count_show.php
@@ -129,7 +129,7 @@ foreach($papers as $pap)
          $query = $this->db->query($sql);
          $count = $query->result_array();
 
-          $sql_back="SELECT count(*) as cnt FROM `backlog_exam_form_report` as `e` JOIN `backlog_student_report` as `s` ON `e`.`student_id` = `s`.`student_id` AND   `s`.`class_id` = `e`.`class_id` AND s.id=e.backlog_student_id WHERE  `s`.`exam_center_id`='".$exam_center."'   AND  `e`.`paper_code` = '".$paper->paper_code."' AND `s`.`class_id` = '".$paper->class_id."' AND   `s`.`exam_center_id` = '".$exam_center."' AND exam_form!='D' AND `e`.`status`='B'  AND s.exam_year='June 2023'";
+          $sql_back="SELECT count(*) as cnt FROM `backlog_exam_form_report` as `e` JOIN `backlog_student_report` as `s` ON `e`.`student_id` = `s`.`student_id` AND   `s`.`class_id` = `e`.`class_id` AND s.id=e.backlog_student_id WHERE  `s`.`exam_center_id`='".$exam_center."'   AND  `e`.`paper_code` = '".$paper->paper_code."' AND `s`.`class_id` = '".$paper->class_id."' AND   `s`.`exam_center_code` = '".$exam_center."' AND exam_form!='D' AND `e`.`status`='B'  AND s.exam_year='June 2024'";
        
       //$sql_back="SELECT count(*) as cnt FROM `backlog_exam_form` as `e` JOIN `backlog_student` as `s` ON `e`.`student_id` = `s`.`student_id` AND   `s`.`class_id` = `e`.`class_id` AND s.id=e.backlog_student_id WHERE  `s`.`exam_center_id`='".$exam_center."'   AND  `e`.`paper_code` = '".$paper->paper_code."' AND `s`.`class_id` = '".$paper->class_id."' AND   `s`.`exam_center_id` = '".$exam_center."' AND exam_form='Y' AND `e`.`status`='B' and s.class_id  in (193,195,197,199,201,203,205,207,209,211,213,302,275,279,221,223,225,227) AND s.exam_year='Dec 2023'";
 
