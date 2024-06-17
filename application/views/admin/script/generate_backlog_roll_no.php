98995ae22fa105bad64defde21e57808d863ede8
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-06-17 17:47:08 +0530

    Update generate_backlog_roll_no.php

--- application/views/admin/script/generate_backlog_roll_no.php
+++ application/views/admin/script/generate_backlog_roll_no.php
@@ -38,14 +38,14 @@
 						//  $this->db->where_in('class_id',array(152,154,155,158,160,162,163,164,165,166,167,172,173,174,175,177,178,180,181,182,193,195,197,199,201,203,205,207,209,211,213,215,217,221,223,225,227,229,231,233,235,237,239,241,243,245,247,249,251,253,255,257,261,263,267,269,275,277,279,281,299,302));
                         //$this->db->where_in('class_id',array(166,167,267,164,165,162,163,169,170,171,174,175,177,178,168,180,173,269));
 						$students = $this->Common_model->getRecordByWhereByOrder('backlog_student',$where,'center_id,center_code,class_id,course_group_id','ASC');
-						$whereRollNo = "exam_form = 'Y' and roll_no !='0' and exam_year = 'Dec 2023'";
+						$whereRollNo = "exam_form = 'Y' and roll_no !='0' and exam_year = 'June 2024'";
 						$countData = $this->db->query("Select max(substr(`roll_no`, 3, 6)) as afterRemove from backlog_student WHERE $whereRollNo")->row();
 						$count = $countData->afterRemove;
 						$last_number = ($count==0) ? '1001'  : $count+1;
 						foreach ($students as $student) {
 								$roll_no = ($student->mode=='REG') ? '91'.$last_number : '92'.$last_number;
 							if($action=='generate'){
-								$whereUpdate = array('student_id' => $student->student_id,'exam_year'=>'Dec 2023','class_id' => $student->class_id);
+								$whereUpdate = array('student_id' => $student->student_id,'exam_year'=>'June 2024','class_id' => $student->class_id);
 								$updateData = array('roll_no' =>$roll_no);
 								$this->Common_model->updateRecordByConditions('backlog_student',$whereUpdate,$updateData);
 							}
