989add8985438f989ce2170a6e545628c44f4756
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-06-20 17:05:48 +0530

    working

--- application/views/admin/examController/paper_details.php
+++ application/views/admin/examController/paper_details.php
@@ -9,11 +9,11 @@
 				<th>Paper Code</th>
 				<th>Paper Name</th>
 				<th>Type</th>
-				<th>CE</th>
-				<th>Max Theory Marks</th>
+				<!-- <th>CE</th> -->
+				<!-- <th>Max Theory Marks</th>
 				<th>Min Theory Marks</th>
 				<th>Max Internal Marks</th>
-				<th>Min Internal Marks</th>
+				<th>Min Internal Marks</th> -->
                <th>Exam Date</th>
 				<th>Exam Day</th>
 				<th>Exam Time</th>
@@ -33,11 +33,11 @@
 					<td><?=$paper["paper_code"]?></td>
 					<td><?=$paper["paper_name"]?></td>
 					<td><?=$paper["type"]?></td>
-					<td><?=$paper["ce"]?></td>
-					<td><?=$paper["max_theory_marks"]?></td>
+					<!-- <td><?=$paper["ce"]?></td> -->
+					<!-- <td><?=$paper["max_theory_marks"]?></td>
 					<td><?=$paper["min_theory_marks"]?></td>
 					<td><?=$paper["max_internal_marks"]?></td>
-					<td><?=$paper["min_internal_marks"]?></td>
+					<td><?=$paper["min_internal_marks"]?></td> -->
                      <td>
 						<?php 
 						if($paper["exam_date"] != "0000-00-00" ){
