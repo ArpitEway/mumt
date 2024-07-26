8852bf28f2cdf36b8e25087d0c911d22c8488724
ShoyebEway <111179622+ShoyebEway@users.noreply.github.com>
2024-07-26 12:03:36 +0530

    Update dashboard.php

--- application/views/Centers/dashboard.php
+++ application/views/Centers/dashboard.php
@@ -271,12 +271,12 @@
 								<span class="nav-text">Result(June 2024)</span>
 							</div>
 						</a>
-						 <a class="border-0 custom-menu-item" href="<?=base_url('backlog_result');?>">
+						 <!-- <a class="border-0 custom-menu-item" href="<?=base_url('backlog_result');?>">
 							<div>
 								<span class="nav-text">Backlog Result (January 2024)</span>
 							</div>
 						</a> 
-						
+						 -->
 						<?php endif ?>
 
 						<!-- <a class="border-0 custom-menu-item" href="<?=base_url('support_system_complaint');?>">
