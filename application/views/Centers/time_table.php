5d1f4aed1822d8a7bfe45b00264c40870a6948d6
neerajeway <106428481+neerajeway@users.noreply.github.com>
2024-08-22 18:19:00 +0530

    Update time_table.php

--- application/views/Centers/time_table.php
+++ application/views/Centers/time_table.php
@@ -1,12 +1,16 @@
 <div id="ss">
     <h3 align="center">&nbsp;</h3>
     <h3 align="center" class="style1" style="text-align:center;font-size:24px;">Maharishi Mahesh Yogi Vedic Vishwavidyalaya  </h3>
-    <p style="text-align:center;font-size:12px;"><strong>Programme for Annual/Semester - Main/Backlog Examination June 2024 </strong></p>
+    
     <!-- <p style="text-align:center;font-size:12px;">(Regular/Private)</p> -->
     <!-- <p style="text-align:center;font-size:12px;"><strong>Early Morning Shift Time 07:00 AM To 10:00 AM </strong></p> -->
-
+    <?php if($paper_list[0]['course_group_id']==45){ ?> 
+        <p style="text-align:center;font-size:12px;"><strong>Programme for Annual/Semester - Main/Backlog Examination August 2024 </strong></p>
+        <p style="text-align:center;font-size:12px;"><strong> Morning Shift Time 10:00 AM To 01:00 PM </strong></p>
+    <?php }else{  ?> 
+        <p style="text-align:center;font-size:12px;"><strong>Programme for Annual/Semester - Main/Backlog Examination June 2024 </strong></p>
     <p style="text-align:center;font-size:12px;"><strong> Morning Shift Time 11:00 AM To 02:00 PM </strong></p>
-    <?php if($paper_list[0]['course_group_id']!=75 && $paper_list[0]['course_group_id']!=76 && $paper_list[0]['course_group_id']!=77){  ?>
+    <?php } if($paper_list[0]['course_group_id']!=75 && $paper_list[0]['course_group_id']!=76 && $paper_list[0]['course_group_id']!=77){  ?>
     <p style="text-align:center;font-size:12px;"><strong> Afternoon Shift Time 03:00 PM To 06:00 PM </strong></p>
     <?php }else{  ?>
     <p style="text-align:center;font-size:12px;"><strong> Afternoon Shift Time 12:00 PM To 03:00 PM </strong></p> <?php } ?>
