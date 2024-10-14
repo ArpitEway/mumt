37004363a276217b506d530f4ae2504f8e7eb54c
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2024-10-14 12:40:59 +0530

    Update Postexam.php

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -28,8 +28,8 @@ class Postexam extends CI_Controller {
             252 - Dec 2025
         */
             $this->db->order_by('center_id,course_group_id,class_id,roll_no');
-            $data['students'] = $this->Common_model->getRecordByWhere('student', array('new_exam_form'=>'Y' ,'roll_no!='=>0 ,'marksheet_no'=>'','class_id'=>'270'));
-            // M10161241YEDU
+            $data['students'] = $this->Common_model->getRecordByWhere('student', array('new_exam_form'=>'Y' ,'roll_no!='=>0 ,'marksheet_no'=>''));
+            // M10161241YEDU ,'class_id'=>'270'
             // $starting_no = 10001 ;
             $starting_no = 10168;
             foreach($data['students']  as $key =>  $student){
