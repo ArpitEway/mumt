6868465340107a96a98eb02e9ddccbc0c35b5957
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2024-12-11 18:52:29 +0530

    Update class_wise_result_upload_status.php

--- application/views/admin/class_wise_result_upload_status.php
+++ application/views/admin/class_wise_result_upload_status.php
@@ -31,7 +31,7 @@ color:red;
                 <?php 
                 $this->db->order_by('mode','desc');
                 $this->db->order_by('class_name','ASC');
-                $this->db->where_in('id',array(101,106,107,108,109,110,111,117,128,134,135,136,161,165,171,174,177,180,194,283,286,200,202,288,204,238,206,208,210,244,291,294,216,303,276,280,222,248,296,224,250,228));
+                //$this->db->where_in('id',array(101,106,107,108,109,110,111,117,128,134,135,136,161,165,171,174,177,180,194,283,286,200,202,288,204,238,206,208,210,244,291,294,216,303,276,280,222,248,296,224,250,228));
                 $class_master = $this->db->get_where('class_master', array('course_group_id' => $course_detail['course_group_id'] ,'exam_form_permission' => 'Y'))->result_array();
                 //exam_form_permission ,'' => 'Y'
              
