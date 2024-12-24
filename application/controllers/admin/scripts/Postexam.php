6664e0f1f85ad54aa99029c7826d2f80d4473c68
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2024-12-24 17:16:59 +0530

    Update Postexam.php

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -59,7 +59,7 @@ class Postexam extends CI_Controller {
             $this->db->select('course_name,student.class_name,class_id, COUNT(student_id) as cnt,student.university_mode,student.exam_pattern');
             $this->db->join('class_master', 'student.class_id = class_master.id');
             // $this->db->where('cbcs', 'Y');
-             $this->db->where('last_class', 'L');
+             // $this->db->where('last_class', 'L');
             //$this->db->where('mode', 'Semester');
             $this->db->where('exam_form', 'Y');
             $this->db->where('upload_result', 'N');
