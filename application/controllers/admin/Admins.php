b296a7603b25761615e72611f53476f9c5718905
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-02-03 17:58:13 +0530

    Update Admins.php

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -3503,8 +3503,8 @@ public function update_exam_datewise_permission(){
 		}else{
 			$this->db->order_by('center_id,roll_number','ASC');
 			// $this->db->limit(1);
-		  $this->db->where('student_id = "691745"');
-         
+		  $this->db->where_in('student_id',array(691745,699903));	
+		         
           if((in_array($class_id, $class_ids))){
               $this->db->where_in('center_id',$dept_ids);
           }
