1583f799d28ce9d43a74c4c52ff0efdcf69d8843
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-03-31 15:54:50 +0530

    Update Postexam.php

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -496,8 +496,8 @@ class Postexam extends CI_Controller {
         // $this->db->where_not_in('id',array('154','172','181'));
         // $this->db->where_in('id',array('300','301'));
         // $this->db->where('old_exam_form_permission','Y');
-        $this->db->where_in('class_name',array('III Year','II Year','I Year'));
-        $this->db->where_not_in('id',array('155','182'));
+        $this->db->where_not_in('class_name',array('III Year','II Year','I Year'));
+        $this->db->where_not_in('id',array('155','299','182'));
         $this->db->where('exam_form_permission','Y');
         $classes = $this->db->get()->result();
         $class_id = array_column($classes,'id');
@@ -549,7 +549,7 @@ class Postexam extends CI_Controller {
             $data = array('demo'=>'Y','new_exam_form'=>'N');
           }
        
-        $update =$this->Common_model->updateRecordByConditions('student',$where,$data);
+        // $update =$this->Common_model->updateRecordByConditions('student',$where,$data);
         $this->Common_model->last_query();
         
         // if($update){
