71888e3e3b283a9293bdc4db7880ba9bef65bde4
Shoaib Shaikh <shoyeb.eway@gmail.com>
2025-04-16 16:33:51 +0530

    Update Otherscript.php

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -1122,7 +1122,14 @@ public function update_roll_no_old_data(){
 		//	break;
 		}
 	}
-	public function dg_locker_data(){
+	public function dg_locker_data($startlimit=0){
+		$start=0;
+		if($startlimit==!0){
+			$start=($startlimit-1)*1000;
+			$this->db->limit(1000,$start);
+			$pagetitle=$startlimit;
+		}
+
 		$exam_year="June 2024";
 		//and enrollment_no in ('AG/21204765','AG/21204615')
 		$this->load->view('header',array('title' => 'Student Data For DIGI LOCKER '.$exam_year));
@@ -1130,7 +1137,7 @@ public function update_roll_no_old_data(){
 		//$sql="SELECT * FROM `old_exam_data` WHERE class_id in (131,125,119,116,110,101,134,107,104) and exam_year in ('August 2022','Aug 2022') and center_id in (21,22,23,24,25,26,27,28)   and university_mode='REG'";
 		
 		$class_ids='101,104,107,110,116,119,125,128,131,134,102,105,108,111,117,120,126,129,132,135,103,106,109,112,118,121,127,130,133,136';
-		 $sql="SELECT * FROM `old_exam_data` WHERE class_id in ($class_ids) and exam_year in ('".$exam_year."') and exam_result !='FAIL' and marks_pattern='GRADE' and  exam_status='R' and university_mode='REG' order by course_name,class_id,roll_no";
+		 $sql="SELECT * FROM `old_exam_data` WHERE class_id in ($class_ids) and exam_year in ('".$exam_year."') and exam_result !='FAIL' and marks_pattern='GRADE' and  exam_status='R' and university_mode='PVT' order by course_name,class_id,roll_no";
 		$rs = $this->db->query($sql)->result_array();
 		$i=1;
 		$this->load->model('Gradesheet_old_model');
