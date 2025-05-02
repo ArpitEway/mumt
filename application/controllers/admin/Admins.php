1295617aab592dc676006d806145c9c9cc7a6484
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-05-02 11:42:20 +0530

    Update Admins.php

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -2325,7 +2325,7 @@ public function getStudentData()
 			'hash_csrf' => $this->security->get_csrf_hash(),
 		);
 		//,'mode'=>'PVT'
-		$where = array('exam_form !=' =>'D','exam_year'=>'June 2025');
+		$where = array('exam_form !=' =>'D','exam_year'=>'June 2025','mode'=>'PVT');
 		$data['counts']=$this->Common_model->backlog_exam_form_permission_status($where);
 		$this->load->view('admin/class_wise_backlog_exam_from_status',$data);
 		$this->load->view('footer');
