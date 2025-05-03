20488cdf48634ecda72bb620366b712edc890574
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-05-03 16:05:52 +0530

    Update Center.php

--- application/controllers/center/Center.php
+++ application/controllers/center/Center.php
@@ -2533,7 +2533,7 @@ class Center extends CI_Controller {
 			//|| ($student[0]->class_id==106 && $student[0]->exam_pattern=='MARKS' )
 			$this->load->view('Centers/marksheet_without_int',$data);
 		}else{
-			if($student[0]->old_class_id=='168' || $student[0]->old_class_id=='256'){
+			if($student[0]->old_class_id=='168' || $student[0]->old_class_id=='256' || $student[0]->old_class_id=='257'){
 				$this->load->view('Centers/marksheet_mom',$data);
 			}else{
 				$this->load->view('Centers/marksheet_bottom',$data);
