23474aadb9757801b0bc02dbf4dc72f20b887853
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-05-20 16:53:09 +0530

    Update ExamController.php

--- application/controllers/admin/ExamController.php
+++ application/controllers/admin/ExamController.php
@@ -1493,8 +1493,8 @@ class ExamController extends CI_Controller {
 			$data['hash_csrf'] = $this->security->get_csrf_hash();
 			$this->db->select('*');
 			$this->db->from('exam_center');
-			$this->db->where_in('examcentercode',array('MDE163','MDE165'));
-			//$this->db->where('examcentercode','MDE165');
+			// $this->db->where_in('examcentercode',array('MDE163','MDE165'));
+			// $this->db->where('examcentercode','MDE165');
 			$this->db->order_by('examcentercode', "asc");
 			$data['exam_centers'] = $this->db->get()->result();
 			$this->db->select('*');
