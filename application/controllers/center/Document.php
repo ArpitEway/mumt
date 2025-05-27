34055b8038293dab86073d1a1557b05275e4050d
Shoaib Shaikh <shoyeb.eway@gmail.com>
2025-05-27 15:54:10 +0530

    Update Document.php

--- application/controllers/center/Document.php
+++ application/controllers/center/Document.php
@@ -38,7 +38,7 @@
 			$where .="student.university_mode='".$course_type."' AND ";	
 		
 		
-			if($this->session->center_id == 2134) {
+			if($this->session->center_id == 2134 || $this->session->center_id == 2050) { //IC1259, IC1339
  				$where.= "document_uploaded!='Y' and payment_status='Y' and center_id=".$this->session->center_id ."  and student.class_name not like '%SEM%' and student.session='July 2024'";
 			}else{
 						$permission_session= $this->Common_model->getRecordByWhere('session',array('document_permission'=>'Y' )); 
