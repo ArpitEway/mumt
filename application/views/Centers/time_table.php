6040dff87ba023ee59d85642b5c907dcf76613a6
Shoaib Shaikh <shoyeb.eway@gmail.com>
2025-06-25 16:02:39 +0530

    Update time_table.php

--- application/views/Centers/time_table.php
+++ application/views/Centers/time_table.php
@@ -4,7 +4,7 @@
     
     <!-- <p style="text-align:center;font-size:12px;">(Regular/Private)</p> -->
     <!-- <p style="text-align:center;font-size:12px;"><strong>Early Morning Shift Time 07:00 AM To 10:00 AM </strong></p> -->
-    <p style="text-align:center;font-size:12px;"><strong>Programme for Annual/Semester - Main/Backlog Examination June 2025 </strong></p>
+    <p style="text-align:center;font-size:12px;"><strong>Programme for Annual/Semester - Main/Backlog Examination <?= (in_array($paper_list[0]['class_id'], array(137,149,183,185,191,138,184,192,187,143,146,139,144,188,145,147,148,150,186,141,151,142,190))) ? 'July' : 'June' ?> 2025 </strong></p>
     <p style="text-align:center;font-size:12px;"><strong> Morning Shift Time 10:00 AM To 01:00 PM </strong></p>
 
     <!-- <p style="text-align:center;font-size:12px;"><strong> Afternoon Shift Time 02:00 PM To 05:00 PM </strong></p> -->
