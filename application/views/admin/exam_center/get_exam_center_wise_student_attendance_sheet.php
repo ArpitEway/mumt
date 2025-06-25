667b38e6f32b5a26ecd6d472607653c06896822b
Shoaib Shaikh <shoyeb.eway@gmail.com>
2025-06-25 15:59:37 +0530

    update time table

--- application/views/admin/exam_center/get_exam_center_wise_student_attendance_sheet.php
+++ application/views/admin/exam_center/get_exam_center_wise_student_attendance_sheet.php
@@ -183,7 +183,7 @@
 					<td><?php echo $i ; ?></td>
 					<td><?php echo date("d-m-Y", strtotime(($student->university_mode == 'PVT' && in_array($student->class_id,$classIdsRegOnly))?$paper->pvt_exam_date:$paper->exam_date)); ?></td>
 					<td><?php 
-					$class_ids = array(104,101,107,110,116,119,273,125,128,131,134,162,163,164,165,283,285,287,289,310,291,293,295,274,297,168,169,170,171,214,106,103,109,112,118,121,127,130,133,136,264);
+					$class_ids = array(104,101,107,110,116,119,273,125,128,131,134,162,163,164,165,283,285,287,289,310,291,293,295,274,297,168,169,170,171,214,106,103,109,112,118,121,127,130,133,136,264,137,149,183,185,191,138,184,192,187,143,146,139,144,188,145,147,148,150,186,141,151,142,190);
 				if($student->university_mode == 'PVT' && in_array($student->class_id,$classIdsRegOnly)){
 							if($paper->pvt_exam_shift=='Afternoon' && in_array($student->class_id,$class_ids)){
 									echo '3:00 PM To 6:00 PM';		
