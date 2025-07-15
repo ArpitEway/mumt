8a67c8b968a868fc4ad8f5ab7f95d684a851b521
Shoaib Shaikh <shoyeb.eway@gmail.com>
2025-07-15 14:41:59 +0530

    Update Center.php

--- application/controllers/center/Center.php
+++ application/controllers/center/Center.php
@@ -1439,9 +1439,19 @@ class Center extends CI_Controller {
 		}
 		$this->db->order_by('id');
 		if($student['university_mode'] != "PVT"){
-		
+		if($student['session'] >='July 2024' && $student['class_id'] == 268){
+			$this->db->where('paper_pattern','NEW');
+		}elseif($student['class_id'] == 268){
+			$this->db->where('paper_pattern','OLD');
+		}
 		$compulsoryPapers = $this->Common_model->get_record('paper_master','*','class_id='.$student['class_id'].' and ce="compulsory" and cbcs_paper="'.$cbcs.'"');
-		$groupPaper = $this->db->query('select p.*,g.group_name from `group` as g join group_paper as p  on g.id=p.group_id join paper_master as m on m.id=p.paper_id where g.class_id='.$student['class_id'].' and cbcs_paper="'.$cbcs.'"  Order by g.id,p.sub_group_id,p.id')->result();
+		$condition = '';
+		if($student['session'] >='July 2024' && $student['class_id'] == 268){
+			$condition = ' and group_pattern="NEW"';
+		}elseif($student['class_id'] ==268){
+			$condition = ' and group_pattern="OLD"';
+		}
+		$groupPaper = $this->db->query('select p.*,g.group_name from `group` as g join group_paper as p  on g.id=p.group_id join paper_master as m on m.id=p.paper_id where g.class_id='.$student['class_id'].' and cbcs_paper="'.$cbcs.'" '.$condition.' Order by g.id,p.sub_group_id,p.id')->result();
 		//echo $this->Common_model->last_query();
 		}else{
 			$compulsoryPapers = $this->Common_model->get_record('paper_master','*','class_id='.$student['class_id'].' and ce="compulsory" and type="theory" and cbcs_paper="'.$cbcs.'"');
