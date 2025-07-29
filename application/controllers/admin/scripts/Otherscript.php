efeb7f2a0ff7ab7d9b0f0c5c58c9ca3d172dfbfd
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-07-29 13:17:43 +0530

    Update Otherscript.php

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -61,7 +61,7 @@ class Otherscript extends CI_Controller {
 		// $where = " paper_code in ('2RBED6')";
 		//$where = " paper_code in ('1RMLIS10','1RMLIS11')";
 		//$where="`paper_code` in ('1RCMSCCH7','1RCMSCCH8','1RCMSCCH9','1RCMSCBT7','1RCMSCBT8','1RCMSCC7','1RCMSCM7','3RMSCC6','3RMSCC7')";
-		$where="`id` in (924,976,1045)";
+		$where="`id` in (947,985,1054)";
 		// 1829,1830,1831 // 382,392,292
 		//in (947,1799,985,1800,1054,1801) (1818,1819,1820,1821)";
 		// $where="`id` = 292";
@@ -22485,6 +22485,7 @@ class Otherscript extends CI_Controller {
 			
 			//student_id not in (703394,703395,683403,685044,685047,686312,689208,702853,723869,724934)
 			$students = $this->Common_model->get_record('student','*',$where);
+
 			$studentData = array(
 				'course_group_id' => $paper['course_group_id'],
 				'class_id' => $paper['class_id'],
