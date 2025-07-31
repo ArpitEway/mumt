f5e094c30538c786dc622032b6ad4e782ecd0fce
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-07-31 11:58:45 +0530

    Update Otherscript.php

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -61,7 +61,7 @@ class Otherscript extends CI_Controller {
 		// $where = " paper_code in ('2RBED6')";
 		//$where = " paper_code in ('1RMLIS10','1RMLIS11')";
 		//$where="`paper_code` in ('1RCMSCCH7','1RCMSCCH8','1RCMSCCH9','1RCMSCBT7','1RCMSCBT8','1RCMSCC7','1RCMSCM7','3RMSCC6','3RMSCC7')";
-		$where="`id` in (947,985)";
+		$where="`id` in (0)";
 		// 1491,1501,1579
 		// 1829,1830,1831 // 382,392,292 // 947,985,1054
 		//in (947,1799,985,1800,1054,1801) (1818,1819,1820,1821)";
@@ -69,7 +69,7 @@ class Otherscript extends CI_Controller {
 		$papers = $this->Common_model->get_record('paper_master','*',$where);
 
 		foreach ($papers as $paper) {
-			$where = ' class_id = "'.$paper['class_id'].'" and temp_exam_form="Y"  and university_mode="PVT"  and  exam_pattern="GRADE" and `student_id` in (784383,787712,788098,801109,801953)';
+			$where = ' class_id = "'.$paper['class_id'].'" and temp_exam_form="Y"  and university_mode="PVT"  and  exam_pattern="GRADE" ';
 			// and university_mode="PVT"  and  exam_pattern="GRADE" 
 			
 			//student_id not in (703394,703395,683403,685044,685047,686312,689208,702853,723869,724934)
