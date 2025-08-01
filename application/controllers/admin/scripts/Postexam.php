5de68602729795717d52168576d0ccc93746a86e
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-08-01 16:40:10 +0530

    Update Postexam.php

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -1455,7 +1455,7 @@ public function upload_old_backlog_data_script($class_id="",$mode){
 }
 
     function backlog_student_course_complete(){
-        $exam_year="Dec 2024";
+        $exam_year="June 2024";
         $class_id = 'backlog_student.class_id';
         //   $this->db->select('DISTINCT(backlog_student.course_group_id),'.$class_id.'');
         $this->db->select('backlog_student.course_group_id,'.$class_id.'');
@@ -1478,7 +1478,7 @@ public function upload_old_backlog_data_script($class_id="",$mode){
       }
       public function backlog_student_course_complete_script()
     { //
-            $exam_year="Dec 2024";    
+            $exam_year="June 2024";    
             $class_id =$_POST['class_id'];
             $class= $this->Common_model->getRecordById('class_master',"id",$class_id);
             
@@ -1488,7 +1488,7 @@ public function upload_old_backlog_data_script($class_id="",$mode){
           $this->db->where(array("course_complete"=>'N','backlog_student.upload_result' => 'Y','backlog_student.exam_form'=>'Y','backlog_student.exam_year'=>$exam_year,'backlog_student.class_id'=>$class_id)); 
           $students =$this->db->get()->result_array();
          
-        $classes= $this->Common_model->getRecordByWhere('class_master',array('course_group_id'=>$class->course_group_id,'mode'=>$class->mode));
+        $classes= $this->Common_model->getRecordByWhere('class_master',array('course_group_id'=>$class->course_group_id,'mode'=>$class->mode,'class_name!'=>'IV Year'));
         $class_count = count($classes);  
         $sno =1; 
         foreach($students as $student){
