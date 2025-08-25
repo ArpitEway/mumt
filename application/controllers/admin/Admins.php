ad773e79cbb913fc437ea2b95102122ea63181f8
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-08-25 17:58:22 +0530

    remove class id

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -3356,8 +3356,10 @@ public function update_exam_datewise_permission(){
 		
 		// $where = "id in (select distinct(course_group_id) from student where exam_form = 'Y' )";
 
-        $where = "id in (select distinct(course_group_id) from student where exam_form = 'Y' and class_id in (155,168,182,283,287,289,293,310,295,297,264) )";
+        $where = "id in (select distinct(course_group_id) from student where exam_form = 'Y' )";
 
+
+        // and class_id in (155,168,182,283,287,289,293,310,295,297,264)
         // 155,182,264,168,289,287,310,293,283,297,295
         // 218,232,234,236,238,240,242,244,246,216,305,278,282,248,250,252,254
 		// 193,217,197,231,203,237,211,275,277,279,281,221,247,223,249,225,251,263 
