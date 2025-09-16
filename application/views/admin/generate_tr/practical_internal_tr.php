23a984489bbbe2bf5ab256b19790c6fcf0bacdd1
Shoaib Shaikh <shoyeb.eway@gmail.com>
2025-09-16 13:01:37 +0530

    update center result

--- application/views/admin/generate_tr/practical_internal_tr.php
+++ application/views/admin/generate_tr/practical_internal_tr.php
@@ -830,7 +830,8 @@ table.last_table, .last_table td, .last_table th{
  
   <?php  
   $dept_ids = array(10,13,20,21,22,23,24,25,26,27,28,29,30);
-  if($final_class && $isFinalClass == false && (!in_array($student->center_id,$dept_ids) || in_array($student->old_class_id ,[103,121,112]))){
+  if($final_class && $isFinalClass == false ){
+    //&& (!in_array($student->center_id,$dept_ids) || in_array($student->old_class_id ,[103,121,112]))
     $final_rw = 0;
     $final_fail =0;
     
