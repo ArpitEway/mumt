cee26eb2d2d0378f28a5913eaadb6f85bdcdc67b
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-09-16 12:12:16 +0530

    Update Otherscript.php

--- application/controllers/admin/scripts/Otherscript.php
+++ application/controllers/admin/scripts/Otherscript.php
@@ -212,8 +212,8 @@ class Otherscript extends CI_Controller {
 		// $marks = array('85','84','83','82'); 
 		// $marks = array('68','67','66','65');
 		  $marks = array('60','59','58','57');
-		$cls_id=109;
-		$sql = "select * from student where class_id='".$cls_id."' and new_exam_form='Y' and p_marks_sub='N' and roll_no!=0 and university_mode='PVT' order by roll_no limit 100";
+		$cls_id=127;
+		$sql = "select * from student where class_id='".$cls_id."' and new_exam_form='Y' and p_marks_sub='N' and roll_no!=0 and university_mode='REG' order by roll_no limit 100";
 		$this->db->where_not_in('center_id',array(20,21,22,23,24,25,26,27,28,29));
 		$rs = $this->db->query($sql)->result_array();
 		$s_no=1;
@@ -239,8 +239,8 @@ class Otherscript extends CI_Controller {
 	{
 		//$marks = array('166','168','170','165'); 
 		$marks = array('85','84','83','82'); 
-		$cls_id=109;
-		$sql = "select * from student where class_id='".$cls_id."' and new_exam_form='Y' and p_marks_sub='N' and  university_mode='PVT'  and exam_pattern= 'GRADE' and  roll_no!=0 order by roll_no  limit 100";
+		$cls_id=133;
+		$sql = "select * from student where class_id='".$cls_id."' and new_exam_form='Y' and p_marks_sub='N' and  university_mode='REG' and  roll_no!=0 order by roll_no  limit 100";
 		// and exam_pattern= 'GRADE'
 		$this->db->where_not_in('center_id',array(20,21,22,23,24,25,26,27,28,29));
 		$rs = $this->db->query($sql)->result_array();
