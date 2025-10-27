b115cc2679526ccfa80a427c66a4b4002d17fdba
Shoaib Shaikh <shoyeb.eway@gmail.com>
2025-10-27 16:08:32 +0530

    update tr class list

--- application/views/admin/tr_class_list.php
+++ application/views/admin/tr_class_list.php
@@ -11,7 +11,7 @@
 		<?php foreach ($courses as $course){
 			$this->db->order_by('id');
 			
-      	  $this->db->where_in('id', array(105,141,145,147,151,268,270));
+      	//   $this->db->where_in('id', array(105,141,145,147,151,268,270));
         $classes= $this->Common_model->getRecordByWhere('class_master',array("course_group_id"=>$course['id'] , 'exam_form_permission' => 'Y' ));
 	
 		//, 'result_permission' => 'Y'
