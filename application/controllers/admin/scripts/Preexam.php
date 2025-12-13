6873e14c59dfd5236e8192563fe00643e170d508
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-12-13 12:47:40 +0530

    set

--- application/controllers/admin/scripts/Preexam.php
+++ application/controllers/admin/scripts/Preexam.php
@@ -106,7 +106,7 @@ class Preexam extends CI_Controller {
 		$class_ids = $classes[0]['class_id'];
 		
 		$this->db->select('count(class_id) as num,course_name,class_name,class_id');
-		$this->db->where('class_id in ('.$class_ids.') and new_exam_form="D" and new_admission_permission="N" and enrolled = "Y" and session="July 2024" and class_name = "I Year" ');
+		$this->db->where('class_id in ('.$class_ids.') and new_exam_form="D" and new_admission_permission="N" and enrolled = "Y" and session="July 2025" and class_name = "I SEM" ');
 
 		//and (( session="July 2023" and class_name = "I Year") || ( session="Jan 2024" and class_name = "I SEM"))
 		
@@ -132,7 +132,9 @@ class Preexam extends CI_Controller {
 		//'temp_exam_form' => "Y",
 		// $this->db->where('((session="July 2023" and class_name = "I Year") || ( session="Jan 2024" and class_name = "I SEM"))');
 
-		$this->db->where('session="July 2024" and class_name = "I Year"');
+		// $this->db->where('session="July 2024" and class_name = "I Year"');
+
+		$this->db->where('session="July 2025" and class_name = "I SEM"');
 		
 		$students = $this->Common_model->get_record('student','*',$where);
 		// $this->Common_model->last_query();
