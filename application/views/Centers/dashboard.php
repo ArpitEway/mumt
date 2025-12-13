6f8b9afb7bb0403a80afcd12e5e825f898967fa8
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-12-13 13:21:35 +0530

    Update dashboard.php

--- application/views/Centers/dashboard.php
+++ application/views/Centers/dashboard.php
@@ -274,26 +274,26 @@
               //  $count = $this->Common_model->getCountByWhere('student',array('center_id'=>$center->id,'new_exam_form !='=>'D'));
                 //&& $count>0href="<?=base_url('exam_form_students');"
                 if ($center->exam_form_permission=='Y' ): ?>
-				 <a class="border-0 custom-menu-item" id="main-exam" >
+				<!--  <a class="border-0 custom-menu-item" id="main-exam" >
 							<div>
 								<span class="nav-text">Exam Form Dec 2025</span>
 							</div>
 					</a> 
-					<a class="border-0 custom-menu-item" href="<?=base_url('backlog_exam_form_students/notSubmitted/center');?>">
+					<a class="border-0 custom-menu-item" href="<?php // echo base_url('backlog_exam_form_students/notSubmitted/center');?>">
 							<div>
 								<span class="nav-text">Backlog Exam Form Dec 2025</span>
 							</div>
-					</a>  
+					</a>   -->
 					<a class="border-0 custom-menu-item karaundi-exam" data-id="main" >
 							<div>
 								<span class="nav-text">Exam Form Karaundi Dec 2025</span>
 							</div>
 					</a> 
-					<a class="border-0 custom-menu-item karaundi-exam" data-id="backlog">
+					<!-- <a class="border-0 custom-menu-item karaundi-exam" data-id="backlog">
 							<div>
 								<span class="nav-text">Backlog Exam Form Karaundi Dec 2025</span>
 							</div>
-					</a>  
+					</a>   -->
 					<?php endif ?>
 				
 				
@@ -499,7 +499,10 @@ $("#main-exam").click(function (e) {
 
 $(".karaundi-exam").click(function (e) {
 	let exam = $(this).data('id');
-	let html = (exam == 'main') ? '<b> आवश्यक सुचना :- </b>सूचित किया जाता है कि मई - जून 2025 में आयोजित होने वाली परीक्षाएं विश्वविद्यालय के मुख्यालय करौंदी जिला कटनी में आयोजित की जाएगी एवं  परीक्षाओं के लिये परीक्षा आवेदन पत्र भरने की अंतिम तिथि 10 मई 2025 एवं विलम्ब शुल्क 200 रुपये सहित परीक्षा आवेदन पत्र भरने की अंतिम तिथि 22 मई 2025 एवं विलंब शुल्क 500 रुपये सहित परीक्षा आवेदन पत्र भरने की अंतिम तिथि 26 मई 2025 निर्धारित की गयी है। निर्धारित तिथि तक परीक्षा आवेदन पत्र भरने की प्रक्रिया पूर्ण करे|' : '<b> आवश्यक सुचना :- </b>सूचित किया जाता है कि मई - जून 2025 में आयोजित होने वाली परीक्षाएं विश्वविद्यालय के मुख्यालय करौंदी जिला कटनी में आयोजित की जाएगी|';
+
+	let html = (exam == 'main') ? '<b> आवश्यक सुचना :- </b>सूचित किया जाता है कि दिसम्बर 2025 में आयोजित होने वाली परीक्षाएं विश्वविद्यालय के मुख्यालय करौंदी जिला कटनी में आयोजित की जाएगी |';
+
+ 
     Swal.fire({
         html: html,
         icon: "info",
