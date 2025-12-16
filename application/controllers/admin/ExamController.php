511a81110e4a76657e5765f09a92fc1cc9b5d771
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-12-16 17:02:25 +0530

    set

--- application/controllers/admin/ExamController.php
+++ application/controllers/admin/ExamController.php
@@ -1706,7 +1706,7 @@ class ExamController extends CI_Controller {
 			$data['hash_csrf'] = $this->security->get_csrf_hash();
 			$this->db->select('DISTINCT(exam_center_id) ');
 			$this->db->from('student');
-			$where = array('new_exam_form'=>'Y', 'roll_no!=' => 0 ,'notification_no'=>43);
+			$where = array('exam_form'=>'Y', 'roll_no!=' => 0 ,'notification_no'=>43);
 			$this->db->where($where);
 			$ecenters=$this->db->get()->result_array();
 			//print_r($this->db->last_query()); 
@@ -1740,7 +1740,7 @@ class ExamController extends CI_Controller {
 		$this->db->from('student');
 		$this->db->order_by("roll_no", "asc");
 		// if($exam_center!="All")
-		$where = array('exam_center_id'=>$exam_center,'new_exam_form'=>'Y', 'roll_no!=' => 0 ,'notification_no'=>43);
+		$where = array('exam_center_id'=>$exam_center,'exam_form'=>'Y', 'roll_no!=' => 0 ,'notification_no'=>43);
 		$this->db->where($where);	
 		$data['exam_center_students'] = $this->db->get()->result();
 		echo $this->load->view('admin/exam_center/get_exam_center_wise_student_attendance_sheet',$data, TRUE);
