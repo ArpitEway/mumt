7a4b0eee505a9956063099147dcc047a49dcd7a2
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2025-12-16 17:06:42 +0530

    set

--- application/views/admin/exam_center/get_exam_center_wise_student_attendance_sheet.php
+++ application/views/admin/exam_center/get_exam_center_wise_student_attendance_sheet.php
@@ -137,9 +137,9 @@
 								 <td rowspan="4" class="text-center"><img src="<?=$img_url;?>"  width="115px" height="166px" /></td> 
 							</tr>
 							<tr>
-							  <td><b>Course: </b> <?=$student->course_name;?> <?php if($student->class_id!=163 && $student->class_id!=175 ) echo  '('.$student->class_name.')';?> </td>
+							  <td colspan="4"><b>Course: </b> <?=$student->course_name;?> <?php if($student->class_id!=163 && $student->class_id!=175 ) echo  '('.$student->class_name.')';?> </td>
 							  <!-- <td colspan="2"><b>EC Code: </b> <?php //echo $exam[0]->exam_center_user;?></td> -->
-							   <td colspan="2"><b> </b></td>
+							   <!-- <td colspan="2"><b> </b></td> -->
 							</tr>
 							<tr>
 							  <td colspan="3"><b>Student Name: </b> <?=$student->name;?></td>
