dfcf00940d325bd344cf50635b477e61d0e837e7
Shoaib Shaikh <shoyeb.eway@gmail.com>
2025-12-27 13:55:02 +0530

    Update get_exam_center_wise_backlog_student_attendance_sheet.php

--- application/views/admin/exam_center/get_exam_center_wise_backlog_student_attendance_sheet.php
+++ application/views/admin/exam_center/get_exam_center_wise_backlog_student_attendance_sheet.php
@@ -119,9 +119,9 @@
 								 <td rowspan="4" class="text-center"><img src="<?=$img_url;?>"  width="115px" height="166px" /></td> 
 							</tr>
 							<tr>
-							  <td colspan="4"><b>Course: </b> <?=$student->course_name;?> (<?= $this->Common_model->getClassNameByClassId($student->class_id);?>) </td>
-							  <!-- <td colspan="2"><b>EC Code: </b> <?php //echo $exam[0]->exam_center_user;?></td>
-							    -->
+							  <td colspan="2"><b>Course: </b> <?=$student->course_name;?> (<?= $this->Common_model->getClassNameByClassId($student->class_id);?>) </td>
+							  <td colspan="2"><b>EC Code: </b> <?php //echo $exam[0]->exam_center_user;?></td>
+							   
 							  <td colspan="2"><b> </b></td>
 							</tr>
 							<tr>
