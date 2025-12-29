4ea881bf7159beb0d98eeaa764eca9bd9de18bf7
HARSHAL632 <131428869+HARSHAL632@users.noreply.github.com>
2025-12-29 11:20:31 +0530

    Update Account.php

--- application/controllers/admin/Account.php
+++ application/controllers/admin/Account.php
@@ -643,7 +643,7 @@
 			$this->db->where('p.payment','Y');
 			$this->db->where('s.new_exam_form','N');
             $this->db->where('p.fees_head','Exam Fees');
-			$this->db->where('p.exam_session','June 2025');
+			$this->db->where('p.exam_session','Dec 2025');
 			$this->db->where('p.center_id',$center_id); 
 			
 			$complaints = $this->db->get()->result();
