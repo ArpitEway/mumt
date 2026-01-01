502f0a612aa4f04d086fbaadaee6ab337460215c
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2026-01-01 10:31:38 +0530

    set

--- application/views/admin/paper_test_id_list.php
+++ application/views/admin/paper_test_id_list.php
@@ -24,8 +24,8 @@
 		$class_ids = array(104,101,107,110,116,119,273,125,128,131,134,162,163,164,165,283,285,287,289,310,291,293,295,274,297,168,169,170,171,214,106,103,109,112,118,121,127,130,133,136,173,174,175,177,180,264,137,149,183,185,191,138,184,192,187,143,146,139,144,188,145,147,148,150,186,141,151,142,190);
 		$exam_time ="";
 		foreach ($papers as $paper): 
-			$old_paper_master ="";
-			//$old_paper_master = $this->Common_model->getRecordByWhere('paper_master_july_24',array('id'=>$paper->id));
+			// $old_paper_master ="";
+			$old_paper_master = $this->Common_model->getRecordByWhere('paper_master_dec_24',array('id'=>$paper->id));
 			if($paper->exam_shift=='Afternoon' && in_array($paper->class_id,$class_ids)){
 				$exam_time = '3:00 PM To 6:00 PM';		
 			}
@@ -49,16 +49,16 @@
 				<td><?=$paper->type ?></td>
 				<td><?=$paper->ce ?></td>
 				<td><?=$paper->test_id ?></td>
-				<td><?=$paper->exam_date ?></td>
-				<td><?=$paper->exam_shift ?></td>
+				<!-- <td><?=$paper->exam_date ?></td>
+				<td><?=$paper->exam_shift ?></td> -->
 				<!-- <td><?=$paper->pvt_test_id ?></td>
 				<td><?=$paper->pvt_exam_date ?></td>
 				<td><?=$paper->pvt_exam_shift ?></td> -->
 				<td><?=$exam_time ?></td>
 				<td><?=$paper->exam_day ?></td>	
-				<!--<td><?php if(!empty($old_paper_master[0]->exam_date)) echo $old_paper_master[0]->exam_date; ?></td>
+				<td><?php if(!empty($old_paper_master[0]->exam_date)) echo $old_paper_master[0]->exam_date; ?></td>
 				<td><?php if(!empty($old_paper_master[0]->exam_shift)) echo $old_paper_master[0]->exam_shift ?></td>
-				<td><?php if(!empty($old_paper_master[0]->exam_day))  echo $old_paper_master[0]->exam_day ?></td> 	 -->
+				<!-- <td><?php if(!empty($old_paper_master[0]->exam_day))  echo $old_paper_master[0]->exam_day ?></td> 	  -->
 			</tr>
 		<?php endforeach ?>
 	</tbody>
