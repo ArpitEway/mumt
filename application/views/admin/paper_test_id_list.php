ef2fa02fe722d1276e10ed8a3c7972fe9e20b603
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2026-01-19 15:17:04 +0530

    add class id

--- application/views/admin/paper_test_id_list.php
+++ application/views/admin/paper_test_id_list.php
@@ -15,9 +15,9 @@
 			<th>Exam Date</th>
 			<th>Exam Shift</th>
 			<th>Exam Time</th>
-			<th>Exam Day</th>
+			<!-- <th>Exam Day</th>
 			<th>Exam Date</th>
-			<th>Exam Shift</th>
+			<th>Exam Shift</th> -->
 			<th>Paper Pattern</th>
 		</tr>
 	</thead>
@@ -25,12 +25,16 @@
 		<?php $i=1; ?>
 		<?php 
 		$class_ids = array(104,101,107,110,116,119,273,125,128,131,134,162,163,164,165,283,285,287,289,310,291,293,295,274,297,168,169,170,171,214,106,103,109,112,118,121,127,130,133,136,173,174,175,177,180,264,137,149,183,185,191,138,184,192,187,143,146,139,144,188,145,147,148,150,186,141,151,142,190);
+		$pgclass_ids = array(193,197,199,201,203,205,302,207,209,211,213,221,227,275,279,223,225,460,476);
 		$exam_time ="";
 		foreach ($papers as $paper): 
 			// $old_paper_master ="";
 			$old_paper_master = $this->Common_model->getRecordByWhere('paper_master_dec_24',array('id'=>$paper->id));
 			if($paper->exam_shift=='Afternoon' && in_array($paper->class_id,$class_ids)){
 				$exam_time = '3:00 PM To 6:00 PM';		
+			}
+			elseif($paper->exam_shift=='Afternoon' && in_array($paper->class_id,$pgclass_ids)){
+				$exam_time = '12:00 PM To 03:00 PM';		
 			}
 			elseif($paper->exam_shift=='Afternoon'){
 				$exam_time = '2:00 PM To 5:00 PM';
@@ -59,8 +63,8 @@
 				<td><?=$paper->pvt_exam_shift ?></td> -->
 				<td><?=$exam_time ?></td>
 				<td><?=$paper->exam_day ?></td>	
-				<td><?php if(!empty($old_paper_master[0]->exam_date)) echo $old_paper_master[0]->exam_date; ?></td>
-				<td><?php if(!empty($old_paper_master[0]->exam_shift)) echo $old_paper_master[0]->exam_shift ?></td>
+			<!-- 	<td><?php if(!empty($old_paper_master[0]->exam_date)) echo $old_paper_master[0]->exam_date; ?></td>
+				<td><?php if(!empty($old_paper_master[0]->exam_shift)) echo $old_paper_master[0]->exam_shift ?></td> -->
 				<!-- <td><?php if(!empty($old_paper_master[0]->exam_day))  echo $old_paper_master[0]->exam_day ?></td>  -->
 				<td><?=$paper->paper_pattern ?></td>
 			</tr>
