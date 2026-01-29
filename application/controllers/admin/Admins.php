968d4d2aa0f081b56cc19b792bbc7c22deb2cd2f
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2026-01-29 15:38:21 +0530

    Update Admins.php

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -3376,9 +3376,9 @@ public function update_exam_datewise_permission(){
 		
 		// $where = "id in (select distinct(course_group_id) from student where exam_form = 'Y' )";
 
-        $where = "id in (select distinct(course_group_id) from student where exam_form = 'Y'  )";
-
-// and class_id in (103,105,106,108,109,112,121,127,130,133,135,136,148,284,286,288,290,292,294,296,298,311)
+        $where = "id in (select distinct(course_group_id) from student where exam_form = 'Y' and class_id in (103,106,109,112,121,127,130,133,136,148,284,286,288,290,292,294,296,298,311))";
+        
+        // 105,108,135
 
 		// and class_id in (103,105,106,108,109,112,121,127,130,133,135,136,148,284,286,288,290,292,294,296,298,311)
         // 103,112,118,109,121,127,130,133,252
