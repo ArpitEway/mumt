16fb01b6e4069dccccff240deb25169a8bc1b2f0
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2026-02-17 10:20:39 +0530

    TR  & Marksheet B.El.Ed.

--- application/models/GradeSheet_old_model_pg.php
+++ application/models/GradeSheet_old_model_pg.php
@@ -110,6 +110,7 @@ class GradeSheet_old_model_pg extends CI_Model
         $old_data = $this->Common_model->getRecordByWhere('old_exam_data',array('student_id'=>$student_id,'class_id'=>$class_id));
         $papers = $this->Common_model->get_all_old_papers($student_id,$class_id,$exam_id);
 		// get_all_group_papers
+		// echo '<pre>';
 		//  print_r($papers);die;
 		
 		// print_r($this->allclass);die;
