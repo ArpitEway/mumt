98dfc21dfd79bae01f913975074c09a83605e33a
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2026-04-09 12:43:41 +0530

    Set Center Files

--- application/views/Centers/grade_marksheet_pg.php
+++ application/views/Centers/grade_marksheet_pg.php
@@ -52,7 +52,7 @@ th.border.border-dark {
             <th class="border-top-0 text-primary pl-3">Enrollment No.</th>
             <th class="border-top-0"><?php  echo $student->enrollment_no ?></th>
             <th class="border-top-0 text-primary pl-3">Roll No.</th>
-            <th class="border-top-0"><?php echo  $student->roll_no; ?></th>
+            <th class="border-top-0"><?php echo  $student->roll_number; ?></th>
             <th rowspan="4" class="border-top-0 text-center" width="170px" height="180px"><img class="img img-thumbnail" src="<?=base_url('assets/student_image/').$student->session.'/'.$student->photo?>" ></th>
         </tr>
         <tr>
@@ -65,7 +65,7 @@ th.border.border-dark {
             <th class="border-top-0 text-primary pl-3">Course</th>
             <th class="border-top-0"><?php  echo $student->course_name ?></th>
             <th class="border-top-0 text-primary pl-3">Class</th>
-            <th class="border-top-0"><?php  echo $this->Common_model->getClassNameByClassId($student->class_id); ?></th>
+            <th class="border-top-0"><?php  echo $this->Common_model->getClassNameByClassId($student->old_class_id); ?></th>
         </tr>
         <!-- <tr>
             <th class="border-top-0 text-primary pl-3">College</th>
@@ -93,7 +93,7 @@ th.border.border-dark {
         </tr>
         <?php 
        
-        $gradesheetData = $this->Gradesheet_model_pg->view_result($student,$student->student_id,$student->course_group_id,$student->class_id,$student->university_mode);
+        $gradesheetData = $this->Gradesheet_model_pg->view_result($student,$student->student_id,$student->course_group_id,$student->old_class_id,$student->university_mode);
        
          ?>
       </tbody>
@@ -112,7 +112,7 @@ th.border.border-dark {
          
         </tr>
         <?php
-        $classes = $this->Common_model->getRecordByWhere("class_master",array('course_group_id'=>$student->course_group_id,'mode'=>$classData->mode,'id!='=>$student->class_id));
+        $classes = $this->Common_model->getRecordByWhere("class_master",array('course_group_id'=>$student->course_group_id,'mode'=>$classData->mode,'id!='=>$student->old_class_id));
         $total_grade_point = 0;
         $total_course_credit = 0;
         $final_fail=0;
@@ -169,7 +169,7 @@ th.border.border-dark {
    
         <tr>
             <?php
-            $attemp_count = $this->Common_model->getRecordByWhere('old_exam_data', array('student_id'=>$student->student_id,'class_id'=>$student->class_id,'exam_status'=>'B'));
+            $attemp_count = $this->Common_model->getRecordByWhere('old_exam_data', array('student_id'=>$student->student_id,'class_id'=>$student->old_class_id,'exam_status'=>'B'));
             ?>
           <!-- <td>TOTAL CREDIT</td> -->
           <td class="text-center" style="vertical-align: middle;"><?= $romanNumerals[$classData->class_order]?></td>
@@ -216,8 +216,8 @@ th.border.border-dark {
         </tr>
         <tr>
             <?php
-            $class_name = explode(' ', $this->Common_model->getClassNameByClassId($student->class_id));
-            $attemp_count = $this->Common_model->getRecordByWhere('old_exam_data', array('student_id'=>$student->student_id,'class_id'=>$student->class_id,'exam_status'=>'B'));
+            $class_name = explode(' ', $this->Common_model->getClassNameByClassId($student->old_class_id));
+            $attemp_count = $this->Common_model->getRecordByWhere('old_exam_data', array('student_id'=>$student->student_id,'class_id'=>$student->old_class_id,'exam_status'=>'B'));
             ?>
           <!-- <td>TOTAL CREDIT</td> -->
           <td class="text-center" style="vertical-align: middle;"><?= $class_name[0]?></td>
