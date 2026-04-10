16ffaa14caf1088d9d3540c23ba2286f5fc21928
Arpit Rajput <90454332+ArpitEway@users.noreply.github.com>
2026-04-10 15:39:24 +0530

    Update Postexam.php

--- application/controllers/admin/scripts/Postexam.php
+++ application/controllers/admin/scripts/Postexam.php
@@ -335,24 +335,26 @@ class Postexam extends CI_Controller {
     public function promote_student_list(){
        
         $this->db->select('COUNT(*) as cnt, student.course_name, student.class_id, student.course_group_id, student.class_name');
-        //$this->db->from('student_result_june_2024 as student');
+        $this->db->from('student_result_june_2025 as student');
         $this->db->from('student as student');
         $this->db->join('class_master', 'class_master.id = student.class_id');
         $this->db->where('student.exam_form', 'Y');
         $this->db->where('student.old_result_show', 'Y');
         $this->db->where('student.promote', 'N');
-       $this->db->where('class_master.mode', 'Semester');
+        $this->db->where('class_master.mode!=', 'Semester');
         $this->db->where('student.course_complete', 'N');
+        $this->db->where('student.university_mode', 'PVT');
 
         // Grouping the complex OR conditions
-        $this->db->group_start();
-            $this->db->where('class_master.last_class IS NULL');
-            $this->db->or_group_start();
-                $this->db->where('class_master.class_name', 'III Year');
-                $this->db->where_in('class_master.id', [103,106,109,112,118,121,127,130,133,136]);
-                $this->db->where_in('student.center_id', [10,13,20,21,22,23,24,25,26,27,28,29,30]);
-            $this->db->group_end();
-        $this->db->group_end();
+        // $this->db->group_start();
+        //     $this->db->where('class_master.last_class IS NULL');
+        //     $this->db->or_group_start();
+        //         $this->db->where('class_master.class_name', 'III Year');
+        //         $this->db->where_in('class_master.id', [103,106,109,112,118,121,127,130,133,136]);
+        //         $this->db->where_in('student.center_id', [10,13,20,21,22,23,24,25,26,27,28,29,30]);
+        //     $this->db->group_end();
+        // $this->db->group_end();
+
         $this->db->group_by('student.class_id');
 
         $data['courses'] = $this->db->get()->result();
@@ -368,8 +370,10 @@ class Postexam extends CI_Controller {
             'hash_csrf' => $this->security->get_csrf_hash(),
         );//'result_show'=>'Y'
         $this->db->limit(1000);
-           $data['students']= $this->Common_model->getRecordByWhere('student',array('class_id' => $class_id, 'exam_form'=>'Y' , 'promote'=>'N' ,'course_complete'=>'N' ));
-         // $data['students']= $this->Common_model->getRecordByWhere('student_result_june_2024',array('class_id' => $class_id, 'exam_form'=>'Y' , 'promote'=>'N' ,'course_complete'=>'N' ));
+           // $data['students']= $this->Common_model->getRecordByWhere('student',array('class_id' => $class_id, 'exam_form'=>'Y' , 'promote'=>'N' ,'course_complete'=>'N' ));
+
+         $data['students']= $this->Common_model->getRecordByWhere('student_result_june_2025',array('class_id' => $class_id, 'exam_form'=>'Y' , 'promote'=>'N' ,'course_complete'=>'N' ));
+
           $data['course_name']= $this->Common_model->getCourseNameByCourseId($course_group_id);
           $data['class_name']= $this->Common_model->getClassNameByClassId($class_id);
           $data['class_id'] =$class_id ;
@@ -396,7 +400,7 @@ class Postexam extends CI_Controller {
             $where = array('student_id'=>$student_id,'new_exam_form'=>'D');
             $update =$this->Common_model->updateRecordByConditions('student',$where,$data);
             $data_result = array( 'promote' => 'Y');
-            //$update_result =$this->Common_model->updateRecordByConditions('student_result_june_2024',$where,$data_result);
+            $update_result =$this->Common_model->updateRecordByConditions('student_result_june_2025',$where,$data_result);
         }
         if($update){
             redirect(base_url('admin/scripts/Postexam/promote_student_list'));
